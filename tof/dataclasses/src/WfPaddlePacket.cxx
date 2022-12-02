ee9716053a3b4ad1aeb5b286aba3b7894fa72a41
Achim Stoessl <achim.stoessl@gmail.com>
2022-12-01 15:36:25 -1000

    FEATURE[WIP] - paddle packet with waveforms

---
+++ tof/dataclasses/src/WfPaddlePacket.cxx
@@ -0,0 +1,23 @@
+#include "packets/WfPaddlePacket.h"
+
+using namespace GAPS;
+
+std::vector<float> WfPaddlePacket::get_waveform(PADDLE_END side) {
+  std::vector<float> foo;
+  return foo;
+}
+
+std::vector<float> WfPaddlePacket::get_times(PADDLE_END side) {
+  std::vector<float> foo;
+  return foo;
+}
+
+std::vector<unsigned char> WfPaddlePacket::serialize() {
+  std::vector<unsigned char> foo;
+  return foo;
+}
+
+void WfPaddlePacket::from_bytstream(std::vector<unsigned char> bytestream) {
+
+}
+
