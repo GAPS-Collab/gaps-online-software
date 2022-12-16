ceb8747a33d59e221633a892934ebcd3ef9c2e7e
Achim Stoessl <achim.stoessl@gmail.com>
2022-12-15 14:00:32 -1000

    FIX - make sure only waveforms over threashold are compared

--- tof/resources/scripts/compare_code.py
+++ tof/resources/scripts/compare_code.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     filename = '/data0/gfp-data-aug/Aug/run4a/rb2.dat'
     console.print(f'=> Reading file {filename}')
     data = open(filename, 'rb')
-    data = data.read()
+    data = data.read(gt.get_current_blobevent_size()*1000)
     bytestream = []
     for k in data:
         bytestream.append(k)
@@ -33,26 +33,42 @@ if __name__ == '__main__':
 
     threshold = 5
     alltimes = []
-    for k in range(1000):
+
+    event_ids_over_thr = []
+    for k in range(len(allevents)):
          
         ev = allevents[k]
         wave = gt.voltage_calibration(ev, calibrations)
         times = gt.timing_calibration(ev, calibrations) 
+        ch_over_thr = []
         for k in [0,2,4,6]:
             wf = np.array(wave[k])
             if wf[wf > threshold].any():
                 tof_event = pbd.waveforms_to_hit(times[k], wave[k], times[k+1], wave[k+1])
                 alltimes.extend([tof_event.t_at_cfdA, tof_event.t_at_cfdB])
+                ch_over_thr.append(k)
+        if ch_over_thr:
+           event_ids_over_thr.append((ev.event_ctr, ch_over_thr))
+    console.print(f'=> We found {len(event_ids_over_thr)} events with waveforms overthreashold')
 
     # compare to the rust code
     f = tables.open_file("/srv/gaps/gaps-online-software/tof/crusty_kraken/waveforms_2.hdf")
     f.root.waveforms.wf.read()
     hdfdata = f.root.waveforms.wf.read()
     tdcs = []
-    for k in hdfdata:
-        tdcs.append(k['tdcs'][0])
-
-    hist = d.factory.hist1d(alltimes, 100)
-    hist.line()
+    thisevent = 0
+    for k in event_ids_over_thr:
+        event_hdfdata = hdfdata[hdfdata['event_ctr'] == k[0]]
+        if not event_hdfdata:
+            continue
+        for ch in k[1]:
+            print (ch)
+            try:
+                tdcs.append(event_hdfdata['tdcs'][ch])
+            except:
+                print ('fail')
+                continue
+#hist = d.factory.hist1d(alltimes, 100)
+    #hist.line()
     #p.show()
         
