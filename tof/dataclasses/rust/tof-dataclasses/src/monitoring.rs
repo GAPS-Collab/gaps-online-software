5445c0e2630edc82d7ba63c55a0390c238971845
Achim Stoessl <achim.stoessl@gmail.com>
2023-06-07 03:14:02 -1000

    FEATURE - extend RBMoniData with all the necessary fields

--- tof/dataclasses/rust/tof-dataclasses/src/monitoring.rs
+++ tof/dataclasses/rust/tof-dataclasses/src/monitoring.rs
@@ -17,11 +17,52 @@ use crate::serialization::{Serialization,
                            parse_f32};
 
 /// A collection of monitoring data
-/// from the readoutboards
+/// from the readoutboards. This includes
+/// temperatures, power data, pressure, humidity
+/// as well as the magnetic sensors
 pub struct RBMoniData {
 
-  pub rate : u32,
-
+  pub board_id           : u8,
+  pub rate               : u16,
+  pub tmp_drc            : f32,
+  pub tmp_clk            : f32,
+  pub tmp_adc            : f32,
+  pub tmp_zynq           : f32,
+  pub tmp_lis3mdltr      : f32,
+  pub tmp_bm280          : f32,
+  pub pressure           : f32,
+  pub humidity           : f32,
+  pub mag_x              : f32,
+  pub mag_y              : f32,
+  pub mag_z              : f32,
+  pub mag_tot            : f32,
+  // voltages
+  pub pow_zynq_v         : f32,
+  pub pow_3_3_v          : f32,
+  pub pow_3_5_v          : f32,
+  pub pow_neg_5_v        : f32,
+  pub pow_drs4_dig_2_5_v : f32,
+  pub pow_drs4_an_2_5_v  : f32,
+  pub pow_adc_dig_2_5_v  : f32,
+  pub pow_adc_an_3_0_v   : f32,
+  // current
+  pub pow_zynq_a         : f32,
+  pub pow_3_3_a          : f32,
+  pub pow_3_5_a          : f32,
+  pub pow_neg_5_a        : f32,
+  pub pow_drs4_dig_2_5_a : f32,
+  pub pow_drs4_an_2_5_a  : f32,
+  pub pow_adc_dig_2_5_a  : f32,
+  pub pow_adc_an_3_0_a   : f32,
+  // power
+  pub pow_zynq_p         : f32,
+  pub pow_3_3_p          : f32,
+  pub pow_3_5_p          : f32,
+  pub pow_neg_5_p        : f32,
+  pub pow_drs4_dig_2_5_p : f32,
+  pub pow_drs4_an_2_5_p  : f32,
+  pub pow_adc_dig_2_5_p  : f32,
+  pub pow_adc_an_3_0_p   : f32,
 }
 
 impl RBMoniData {
@@ -34,54 +75,159 @@ impl RBMoniData {
   /// HEAD + TAIL + sum(sizeof(m) for m in _all_members_))
   pub const SIZE : usize  = 6;
 
-  pub fn new() -> RBMoniData {
-    RBMoniData {
-      rate : 0,  
+  pub fn new() -> Self {
+    Self {
+      board_id           : 0, 
+      rate               : 0,
+      tmp_drc            : 0.0,
+      tmp_clk            : 0.0,
+      tmp_adc            : 0.0,
+      tmp_zynq           : 0.0,
+      tmp_lis3mdltr      : 0.0,
+      tmp_bm280          : 0.0,
+      pressure           : 0.0,
+      humidity           : 0.0,
+      mag_x              : 0.0,
+      mag_y              : 0.0,
+      mag_z              : 0.0,
+      mag_tot            : 0.0,
+      // voltages
+      pow_zynq_v         : 0.0,
+      pow_3_3_v          : 0.0,
+      pow_3_5_v          : 0.0,
+      pow_neg_5_v        : 0.0,
+      pow_drs4_dig_2_5_v : 0.0,
+      pow_drs4_an_2_5_v  : 0.0,
+      pow_adc_dig_2_5_v  : 0.0,
+      pow_adc_an_3_0_v   : 0.0,
+      // current
+      pow_zynq_a         : 0.0,
+      pow_3_3_a          : 0.0,
+      pow_3_5_a          : 0.0,
+      pow_neg_5_a        : 0.0,
+      pow_drs4_dig_2_5_a : 0.0,
+      pow_drs4_an_2_5_a  : 0.0,
+      pow_adc_dig_2_5_a  : 0.0,
+      pow_adc_an_3_0_a   : 0.0,
+      // power
+      pow_zynq_p         : 0.0,
+      pow_3_3_p          : 0.0,
+      pow_3_5_p          : 0.0,
+      pow_neg_5_p        : 0.0,
+      pow_drs4_dig_2_5_p : 0.0,
+      pow_drs4_an_2_5_p  : 0.0,
+      pow_adc_dig_2_5_p  : 0.0,
+      pow_adc_an_3_0_p   : 0.0,
     }
-  }
-
-  pub fn to_bytestream(&self) -> Vec<u8> {
-    let mut bytestream = Vec::<u8>::with_capacity(RBMoniData::SIZE);
-    bytestream.extend_from_slice(&RBMoniData::HEAD.to_le_bytes());
-    bytestream.extend_from_slice(&self.rate.to_le_bytes());
-    bytestream.extend_from_slice(&RBMoniData::TAIL.to_le_bytes());
-    bytestream
   }
 }
 
 impl Default for RBMoniData {
-  fn default() -> RBMoniData {
+  fn default() -> Self {
     RBMoniData::new()
   }
 }
 
 impl Serialization for RBMoniData {
+  
+  fn to_bytestream(&self) -> Vec<u8> {
+    let mut stream = Vec::<u8>::with_capacity(RBMoniData::SIZE);
+    stream.extend_from_slice(&RBMoniData::HEAD.to_le_bytes());
+    stream.extend_from_slice(&self.board_id          .to_le_bytes()); 
+    stream.extend_from_slice(&self.rate              .to_le_bytes()); 
+    stream.extend_from_slice(&self.tmp_drc           .to_le_bytes()); 
+    stream.extend_from_slice(&self.tmp_clk           .to_le_bytes()); 
+    stream.extend_from_slice(&self.tmp_adc           .to_le_bytes()); 
+    stream.extend_from_slice(&self.tmp_zynq          .to_le_bytes()); 
+    stream.extend_from_slice(&self.tmp_lis3mdltr     .to_le_bytes()); 
+    stream.extend_from_slice(&self.tmp_bm280         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pressure          .to_le_bytes()); 
+    stream.extend_from_slice(&self.humidity          .to_le_bytes()); 
+    stream.extend_from_slice(&self.mag_x             .to_le_bytes()); 
+    stream.extend_from_slice(&self.mag_y             .to_le_bytes()); 
+    stream.extend_from_slice(&self.mag_z             .to_le_bytes()); 
+    stream.extend_from_slice(&self.mag_tot           .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_zynq_v        .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_3_3_v         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_3_5_v         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_neg_5_v       .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_drs4_dig_2_5_v.to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_drs4_an_2_5_v .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_adc_dig_2_5_v .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_adc_an_3_0_v  .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_zynq_a        .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_3_3_a         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_3_5_a         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_neg_5_a       .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_drs4_dig_2_5_a.to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_drs4_an_2_5_a .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_adc_dig_2_5_a .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_adc_an_3_0_a  .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_zynq_p        .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_3_3_p         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_3_5_p         .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_neg_5_p       .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_drs4_dig_2_5_p.to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_drs4_an_2_5_p .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_adc_dig_2_5_p .to_le_bytes()); 
+    stream.extend_from_slice(&self.pow_adc_an_3_0_p  .to_le_bytes()); 
+    stream.extend_from_slice(&RBMoniData::TAIL.to_le_bytes());
+    stream
+  }
+
   fn from_bytestream(stream    : &Vec<u8>, 
                      pos       : &mut usize) 
     -> Result<RBMoniData, SerializationError>{
+    let mut moni_data = RBMoniData::new();
+    let head_pos = search_for_u16(RBMoniData::HEAD, stream, *pos)?; 
+    let tail_pos = search_for_u16(RBMoniData::TAIL, stream, head_pos + RBMoniData::SIZE-2)?;
+    // At this state, this can be a header or a full event. Check here and
+    // proceed depending on the options
+    if tail_pos + 2 - head_pos != RBMoniData::SIZE {
+      error!("RBMoniData. Seing {} bytes, but expecting {}", tail_pos + 2 - head_pos, RBMoniData::SIZE);
+      *pos = head_pos + 2; 
+      return Err(SerializationError::WrongByteSize);
+    }
 
-    let mut two_bytes : [u8;2];
-    let four_bytes    : [u8;4];
-    two_bytes = [stream[*pos],
-                 stream[*pos+1]];
+    moni_data.board_id           = parse_u8(&stream, pos); 
+    moni_data.rate               = parse_u16(&stream, pos); 
+    moni_data.tmp_drc            = parse_f32(&stream, pos); 
+    moni_data.tmp_clk            = parse_f32(&stream, pos); 
+    moni_data.tmp_adc            = parse_f32(&stream, pos); 
+    moni_data.tmp_zynq           = parse_f32(&stream, pos); 
+    moni_data.tmp_lis3mdltr      = parse_f32(&stream, pos); 
+    moni_data.tmp_bm280          = parse_f32(&stream, pos); 
+    moni_data.pressure           = parse_f32(&stream, pos); 
+    moni_data.humidity           = parse_f32(&stream, pos); 
+    moni_data.mag_x              = parse_f32(&stream, pos); 
+    moni_data.mag_y              = parse_f32(&stream, pos); 
+    moni_data.mag_z              = parse_f32(&stream, pos); 
+    moni_data.mag_tot            = parse_f32(&stream, pos); 
+    moni_data.pow_zynq_v         = parse_f32(&stream, pos); 
+    moni_data.pow_3_3_v          = parse_f32(&stream, pos); 
+    moni_data.pow_3_5_v          = parse_f32(&stream, pos); 
+    moni_data.pow_neg_5_v        = parse_f32(&stream, pos); 
+    moni_data.pow_drs4_dig_2_5_v = parse_f32(&stream, pos); 
+    moni_data.pow_drs4_an_2_5_v  = parse_f32(&stream, pos); 
+    moni_data.pow_adc_dig_2_5_v  = parse_f32(&stream, pos); 
+    moni_data.pow_adc_an_3_0_v   = parse_f32(&stream, pos); 
+    moni_data.pow_zynq_a         = parse_f32(&stream, pos); 
+    moni_data.pow_3_3_a          = parse_f32(&stream, pos); 
+    moni_data.pow_3_5_a          = parse_f32(&stream, pos); 
+    moni_data.pow_neg_5_a        = parse_f32(&stream, pos); 
+    moni_data.pow_drs4_dig_2_5_a = parse_f32(&stream, pos); 
+    moni_data.pow_drs4_an_2_5_a  = parse_f32(&stream, pos); 
+    moni_data.pow_adc_dig_2_5_a  = parse_f32(&stream, pos); 
+    moni_data.pow_adc_an_3_0_a   = parse_f32(&stream, pos); 
+    moni_data.pow_zynq_p         = parse_f32(&stream, pos); 
+    moni_data.pow_3_3_p          = parse_f32(&stream, pos); 
+    moni_data.pow_3_5_p          = parse_f32(&stream, pos); 
+    moni_data.pow_neg_5_p        = parse_f32(&stream, pos); 
+    moni_data.pow_drs4_dig_2_5_p = parse_f32(&stream, pos); 
+    moni_data.pow_drs4_an_2_5_p  = parse_f32(&stream, pos); 
+    moni_data.pow_adc_dig_2_5_p  = parse_f32(&stream, pos); 
+    moni_data.pow_adc_an_3_0_p   = parse_f32(&stream, pos); 
     *pos += 2;
-    if RBMoniData::HEAD != u16::from_le_bytes(two_bytes) {
-      warn!("Packet does not start with HEAD signature");
-      return Err(SerializationError::HeadInvalid {});
-    }
-    four_bytes = [stream[*pos],
-                  stream[*pos+1],
-                  stream[*pos+2],
-                  stream[*pos+3]];
-    *pos += 4;
-    two_bytes = [stream[*pos],
-                 stream[*pos+1]];
-    let mut moni_data  = RBMoniData::new();
-    moni_data.rate = u32::from_le_bytes(four_bytes);  
-    if RBMoniData::TAIL != u16::from_le_bytes(two_bytes) {
-      warn!("Packet does not end with TAIL signature");
-      return Err(SerializationError::TailInvalid {});
-    }
     Ok(moni_data) 
   }
 }
@@ -115,10 +261,10 @@ impl Default for TofCmpMoniData {
 
 impl fmt::Display for TofCmpMoniData {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "<TofCmpMoniData:\n
-           \t CORE1 TMP [C] {}\n
-           \t CORE2 TMP [C] {}\n
-           \t PCH TMP [C] {}>",
+    write!(f, "<TofCmpMoniData:
+           \t CORE1 TMP [C] {}
+           \t CORE2 TMP [C] {}
+           \t PCH TMP   [C] {}>",
            self.core1_tmp, self.core2_tmp, self.pch_tmp)
   }
 }
@@ -193,13 +339,13 @@ impl Default for MtbMoniData {
 
 impl fmt::Display for MtbMoniData {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "<MtbMoniData:\n
-           \t MTB  EVT RATE {}\n
-           \t LOST EVT RATE {}\n
-           \t FPGA TMP     [C] {}\n
-           \t FPGA VCCINT  [V] {}\n
-           \t FPGA VCCAUX  [V] {}\n
-           \t FPGA VCCBRAM [V] {}\n>",
+    write!(f, "<MtbMoniData:
+           \t MTB  EVT RATE [Hz] {}
+           \t LOST EVT RATE [Hz] {}
+           \t FPGA TMP      [C]  {}
+           \t FPGA VCCINT   [V]  {}
+           \t FPGA VCCAUX   [V]  {}
+           \t FPGA VCCBRAM  [V]  {}>",
            self.rate,
            self.lost_rate,
            self.fpga_vccint,
