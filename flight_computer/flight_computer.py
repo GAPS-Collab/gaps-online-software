3a8a64522d546ce49df0d2d596924e433e923d4d
tof-computer <stoessl@hawaii.edu>
2023-06-13 17:13:35 +0000

    CALIBRATION - modification to calibrations scripts, to allow mass calibration

--- flight_computer/flight_computer.py
+++ flight_computer/flight_computer.py
@@ -9,10 +9,12 @@ import gaps_tof as gt
 def print_event(event):
     print (f'event : {event.event_id} n_paddles {event.n_paddles}')
 
+VERBOSE=False
+
 ctx = zmq.Context()
 sock = ctx.socket(zmq.SUB)
 #sock.connect("tcp://127.0.0.1:30000")
-sock.connect("tcp://192.168.36.20:40000")
+sock.connect("tcp://192.168.37.20:40000")
 sock.subscribe("")
 all_events = []
 
@@ -25,24 +27,26 @@ while True:
     data = [k for k in data]
     packet.from_bytestream(data, 0)
     #print (packet)
+    
     if packet.packet_type != gt.PacketType.TofEvent:
         #print (packet.packet_type)
         continue
     if packet.packet_type == gt.PacketType.TofEvent:
-        #print ("Got tof packet")
         event = gt.REventPacket()
         data = [k for k in packet.payload]
         event.from_bytestream(data,0)
-        #print (f".. event {event.event_id}, .. no paddle packets")
-        #if len(data) > 9:
-        #    print (event)
-        #    for k in event.paddle_packets:
-        #        print (k)
         all_events.append(event.event_id)
         #if len(all_events) % 100 == 0:
         #    print ([k.event_id for k in all_events])
         npackets += 1
-        #print (f"received {npackets} packets, delta t {time.time() - now}")
+        if VERBOSE:
+            pass
+            #print (f".. event {event.event_id}, .. no paddle packets")
+            #if len(data) > 9:
+            #    print (event)
+            #    for k in event.paddle_packets:
+            #        print (k)
+            #print (f"received {npackets} packets, delta t {time.time() - now}")
     #if len(data) > 15:
     #    print (f'=======')
     #    print (f' last event {event.event_id} {event.n_paddles}')
@@ -59,20 +63,20 @@ while True:
         #print (event)
         #for k in event.paddle_packets:
         #    print (k)
-        if len(event.paddle_packets) > 4:
-            print(event)
-            for k in event.paddle_packets:
-                print (k)
-            #raise
-        if event.n_paddles > 0:
-            print(event)
-            for k in event.paddle_packets:
-                print (k)
-            #if len(event.paddle_packets) > 1:
-            #    raise
-            #if not event.is_broken():
-            #     print(event.paddle_packets[0])
-            #raise
+        ##if len(event.paddle_packets) > 4:
+        ##    print(event)
+        ##    for k in event.paddle_packets:
+        ##        print (k)
+        ##    #raise
+        ##if event.n_paddles > 0:
+        ##    print(event)
+        ##    for k in event.paddle_packets:
+        ##        print (k)
+        ##    #if len(event.paddle_packets) > 1:
+        ##    #    raise
+        ##    #if not event.is_broken():
+        ##    #     print(event.paddle_packets[0])
+        ##    #raise
         print (f"received {npackets} packets, delta t {time.time() - now}")
         now = time.time()
         all_events = np.array(all_events)
