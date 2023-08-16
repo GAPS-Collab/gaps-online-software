c617ef3fdbad4cb43bf559f28ff5ef979de9ac31
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2023-08-16 08:45:16 +0000

    WIP - adds calibration to unpack-tofpackets example

--- tof/resources/examples/unpack-tofpackets.py
+++ tof/resources/examples/unpack-tofpackets.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 """
 Example: How to read files with tofpackets from the python API
@@ -14,14 +14,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Example on how to read TofPackets from a file.')
     parser.add_argument('tofpacketfile', metavar='packetfile', type=str,
                         help='A file with tofpackets in it')
+    parser.add_argument('--calibration', '-c', dest='calibration',
+                        default='', type=str,
+                        help='Calibration txt file for this specific readout board.')
+    
+    args = parser.parse_args()
+    if args.calibration:
+        calib = gt.RBCalibration.from_txtfile(args.calibration)
     
     packets      = gt.get_tofpackets(args.tofpacketfile)
      
     for pack in packets:
         print (pack)
         ev = gt.TofEvent.from_bytestream(pack.payload, 0)
-        for k in ev.rbevents:
+        for rb_ev in ev.rbevents:
             # event header containing event id etc
-            print (k.header)
+            print (rb_ev.header)
             # get channel adcs
-            k.get_channel_adc(1)
+            rb_ev.get_channel_adc(1)
+            if calib is not None:
+                print (calib.nanoseconds(rb_ev))
+                print (calib.voltages(rb_ev, spike_cleaning=True))
+
