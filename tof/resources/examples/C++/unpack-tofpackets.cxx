e74c7503c1dee5626d11e70d1943e4d7921d3355
Achim Stoessl <achim.stoessl@gmail.com>
2023-09-20 10:22:16 -1000

    ADD - example for reading out tofpackets directly with C++ API
    Code still needs to be added, this is just the file

---
+++ tof/resources/examples/C++/unpack-tofpackets.cxx
@@ -0,0 +1,13 @@
+/**
+ * Binary to unpack tofpackets/raw rb data to illustrate 
+ * how to work with teh API
+ * 
+ * September 2023, gaps-online-sw V0.7
+ * The API will not be stable until V1.0 and is thus 
+ * subject to change. Please refer to the respective 
+ * README.md
+ *
+ */
+
+int main(){
+}
