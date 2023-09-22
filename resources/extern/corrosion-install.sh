f169fd13e8446cad16acf78d2cca1d7ff0d6a3ec
Achim Stoessl <achim.stoessl@gmail.com>
2023-09-21 16:00:50 -1000

    RM - remove outdated stuff

--- resources/extern/corrosion-install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# install script for corrosion, an interlink
-# for cargo in cmake
-cmake -DCMAKE_INSTALL_PREFIX=corrosion/install -Scorrosion -Bcorrosion/build -DCMAKE_BUILD_TYPE=Release 
-cmake --build corrosion/build --config Release
-cmake --install corrosion/build --config Release
