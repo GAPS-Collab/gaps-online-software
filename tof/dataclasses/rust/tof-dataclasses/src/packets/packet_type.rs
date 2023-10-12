902e3f0898bf805679f572437caa5ead7d8e217f
Achim Stoessl <achim.stoessl@gmail.com>
2023-10-11 19:50:35 -1000

    FIX - removes unused SCALAR packet type

--- tof/dataclasses/rust/tof-dataclasses/src/packets/packet_type.rs
+++ tof/dataclasses/rust/tof-dataclasses/src/packets/packet_type.rs
@@ -8,16 +8,16 @@ use std::fmt;
 #[derive(Debug, PartialEq, Clone)]
 pub enum PacketType {
   Unknown       , 
-  Command       ,
   TofEvent      ,
   Monitor       ,
   MasterTrigger , 
   HeartBeat     ,
-  Scalar        ,
   RBEventHeader ,
   RBEvent       ,
   RBEventMemoryView,
-  RBMoni     ,
+  TofCommand    ,
+  RBCommand     ,
+  RBMoni        ,
   MonitorTofCmp ,
   MonitorMtb    ,
   RBCalibration ,
@@ -25,27 +25,26 @@ pub enum PacketType {
 
 impl PacketType {
   pub const UNKNOWN           : u8 =  0;
-  pub const COMMAND           : u8 = 10;
   pub const RBEVENT           : u8 = 20;
   pub const TOFEVENT          : u8 = 21;
   // not specific enough, deprecated. Use the packet
   // types for monitor packets below
   pub const MONITOR           : u8 = 30;
   pub const HEARTBEAT         : u8 = 40;
-  pub const SCALAR            : u8 = 50;
   pub const MT                : u8 = 60;
-  pub const RBEVENTHEADER          : u8 = 70;
+  pub const RBEVENTHEADER     : u8 = 70;
   // monitoring packets
   pub const TOFCMP_MONI       : u8 = 80;
   pub const MTB_MONI          : u8 = 90;
   pub const RB_MONI           : u8 = 100;
   pub const RBEVENTMEMORYVIEW : u8 = 120;
   pub const RBCALIBRATION     : u8 = 130;
+  pub const TOFCOMMAND        : u8 = 140;
+  pub const RBCOMMAND         : u8 = 150;
 
   pub fn as_u8(packet_type : &PacketType)   -> u8 {
     match packet_type {
       PacketType::Unknown           => Self::UNKNOWN,
-      PacketType::Command           => Self::COMMAND,
       PacketType::RBEvent           => Self::RBEVENT,
       PacketType::RBEventHeader          => Self::RBEVENTHEADER,
       PacketType::RBEventMemoryView => Self::RBEVENTMEMORYVIEW,
@@ -53,30 +52,31 @@ impl PacketType {
       PacketType::Monitor           => Self::MONITOR,
       PacketType::HeartBeat         => Self::HEARTBEAT,
       PacketType::MasterTrigger     => Self::MT,
-      PacketType::Scalar            => Self::SCALAR,
       PacketType::RBMoni            => Self::RB_MONI,
       PacketType::MonitorTofCmp     => Self::TOFCMP_MONI,
       PacketType::MonitorMtb        => Self::MTB_MONI,
       PacketType::RBCalibration     => Self::RBCALIBRATION,
+      PacketType::RBCommand         => Self::RBCOMMAND,
+      PacketType::TofCommand        => Self::TOFCOMMAND,
     }
   }
 
   pub fn from_u8(value : u8) -> Option<PacketType> {
     match value {
       Self::UNKNOWN           => Some(PacketType::Unknown),  
-      Self::COMMAND           => Some(PacketType::Command), 
       Self::RBEVENT           => Some(PacketType::RBEvent), 
       Self::TOFEVENT          => Some(PacketType::TofEvent),
       Self::MONITOR           => Some(PacketType::Monitor), 
       Self::HEARTBEAT         => Some(PacketType::HeartBeat),
       Self::MT                => Some(PacketType::MasterTrigger),
-      Self::SCALAR            => Some(PacketType::Scalar),
-      Self::RBEVENTHEADER          => Some(PacketType::RBEventHeader),
+      Self::RBEVENTHEADER     => Some(PacketType::RBEventHeader),
       Self::RBEVENTMEMORYVIEW => Some(PacketType::RBEventMemoryView),
       Self::RB_MONI           => Some(PacketType::RBMoni),
       Self::MTB_MONI          => Some(PacketType::MonitorMtb),
       Self::TOFCMP_MONI       => Some(PacketType::MonitorTofCmp),
       Self::RBCALIBRATION     => Some(PacketType::RBCalibration),
+      Self::TOFCOMMAND        => Some(PacketType::TofCommand),
+      Self::RBCOMMAND         => Some(PacketType::RBCommand),
       _   => None,
     }
   }
@@ -88,19 +88,19 @@ impl fmt::Display for PacketType {
     let repr : String;
     match self {
       PacketType::Unknown           => { repr = String::from("Unknown")     },
-      PacketType::Command           => { repr = String::from("Command")     },
       PacketType::RBEvent           => { repr = String::from("RBEvent")     },
       PacketType::RBEventMemoryView => { repr = String::from("RBEventMemoryView") },
       PacketType::TofEvent          => { repr = String::from("TOFEvent")    },
       PacketType::Monitor           => { repr = String::from("Monitor")     },
       PacketType::HeartBeat         => { repr = String::from("HeartBeat")   },
       PacketType::MasterTrigger     => { repr = String::from("MasterTrigger") },
-      PacketType::Scalar            => { repr = String::from("Scalar")      },
       PacketType::RBEventHeader          => { repr = String::from("RBEventHeader")    },
       PacketType::RBMoni            => { repr = String::from("RBMoni")     },
       PacketType::MonitorTofCmp     => { repr = String::from("TOFCMPMoni") },
       PacketType::MonitorMtb        => { repr = String::from("MTBMoni")    },
       PacketType::RBCalibration     => { repr = String::from("RBCalibration")},
+      PacketType::TofCommand        => { repr = String::from("TofCommand")},
+      PacketType::RBCommand         => { repr = String::from("RBCommand")},
     }
     write!(f, "<PacketType {}>", repr)
   }
@@ -115,7 +115,6 @@ fn test_packet_types() {
   type_codes.push(PacketType::TOFEVENT         ); 
   type_codes.push(PacketType::MONITOR          ); 
   type_codes.push(PacketType::HEARTBEAT        ); 
-  type_codes.push(PacketType::SCALAR           ); 
   type_codes.push(PacketType::MT               ); 
   type_codes.push(PacketType::RBEVENTHEADER    ); 
   type_codes.push(PacketType::TOFCMP_MONI      ); 
@@ -123,6 +122,8 @@ fn test_packet_types() {
   type_codes.push(PacketType::RB_MONI          );
   type_codes.push(PacketType::RBEVENTMEMORYVIEW);
   type_codes.push(PacketType::RBCALIBRATION    );
+  type_codes.push(PacketType::RBCOMMAND        );
+  type_codes.push(PacketType::TOFCOMMAND       );
   for tc in type_codes.iter() {
     assert_eq!(*tc,PacketType::as_u8(&PacketType::from_u8(*tc).unwrap()));  
   }
