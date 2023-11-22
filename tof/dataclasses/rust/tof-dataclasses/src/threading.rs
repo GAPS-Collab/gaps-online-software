9a9504aaa39e8986679a8f1e60c7768294dccb06
Achim Stoessl <achim.stoessl@gmail.com>
2023-11-22 10:02:49 -1000

    REFACTOR - adjust spacing to be more eye-friendly

--- tof/dataclasses/rust/tof-dataclasses/src/threading.rs
+++ tof/dataclasses/rust/tof-dataclasses/src/threading.rs
@@ -19,103 +19,97 @@ pub struct ThreadPool {
 }
 
 trait FnBox {
-    fn call_box(self: Box<Self>);
+  fn call_box(self: Box<Self>);
 }
 
 impl<F: FnOnce()> FnBox for F {
   fn call_box(self: Box<F>) {
-      (*self)()
+    (*self)()
   }
 }
 
 type Job = Box<dyn FnBox + Send + 'static>;
 
 impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+  /// Create a new ThreadPool.
+  ///
+  /// The size is the number of threads in the pool.
+  ///
+  /// # Panics
+  ///
+  /// The `new` function will panic if the size is zero.
+  pub fn new(size: usize) -> ThreadPool {
+    assert!(size > 0);
 
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-        let mut workers = Vec::with_capacity(size);
+    let (sender, receiver) = mpsc::channel();
+    let receiver = Arc::new(Mutex::new(receiver));
+    let mut workers = Vec::with_capacity(size);
 
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool {
-            workers,
-            sender,
-        }
+    for id in 0..size {
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
 
-    pub fn execute<F>(&self, f: F)
-        where
-            F: FnOnce() + Send + 'static
-    {
-        let job = Box::new(f);
-
-        self.sender.send(Message::NewJob(job)).unwrap();
+    ThreadPool {
+      workers,
+      sender,
     }
+  }
+
+  pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static {
+    let job = Box::new(f);
+    self.sender.send(Message::NewJob(job)).unwrap();
+  }
 }
 
 impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        info!("Sending terminate message to all workers.");
+  fn drop(&mut self) {
+    info!("Sending terminate message to all workers.");
 
-        for _ in &mut self.workers {
-            self.sender.send(Message::Terminate).unwrap();
-        }
-
-        warn!("Shutting down all workers.");
-
-        for worker in &mut self.workers {
-            info!("Shutting down worker {}", worker.id);
-
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
+    for _ in &mut self.workers {
+      self.sender.send(Message::Terminate).unwrap();
     }
+
+    warn!("Shutting down all workers.");
+
+    for worker in &mut self.workers {
+      info!("Shutting down worker {}", worker.id);
+
+      if let Some(thread) = worker.thread.take() {
+          thread.join().unwrap();
+      }
+    }
+  }
 }
 
 struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
+  id: usize,
+  thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) ->
-        Worker {
-
-        let thread = thread::spawn(move ||{
-            loop {
-                let message = receiver.lock().unwrap().recv().unwrap();
-
-                match message {
-                    Message::NewJob(job) => {
-                        trace!("Worker {} got a job; executing.", id);
-
-                        job.call_box();
-                    },
-                    Message::Terminate => {
-                        trace!("Worker {} was told to terminate.", id);
-
-                        break;
-                    },
-                }
-            }
-        });
-
-        Worker {
-            id,
-            thread: Some(thread),
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) ->
+    Worker {
+      let thread = thread::spawn(move ||{
+      loop {
+        let message = receiver.lock().unwrap().recv().unwrap();
+        match message {
+          Message::NewJob(job) => {
+            trace!("Worker {} got a job; executing.", id);
+            job.call_box();
+          },
+          Message::Terminate => {
+            trace!("Worker {} was told to terminate.", id);
+            break;
+          },
         }
+      }
+    });
+
+    Worker {
+      id,
+      thread: Some(thread),
     }
+  }
 }
