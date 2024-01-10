1111a5eaf273143a874b48c65fa38f91bd1e63ed
achim1 <achim1@users.noreply.github.com>
2024-01-10 02:48:07 +0000

    Deploying to gh-pages from  @ 7a98e195b721a7df252e4f134f661bdb023309cf 🚀

---
+++ implementors/std/os/fd/owned/trait.AsFd.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"nix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"nix/sys/signalfd/struct.SignalFd.html\" title=\"struct nix::sys::signalfd::SignalFd\">SignalFd</a>"]],
+"rustix":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
