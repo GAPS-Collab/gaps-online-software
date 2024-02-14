f3dff91cc6eb09540a1c68b998ea1494bffb15c8
Achim Stoessl <achim.stoessl@gmail.com>
2024-02-14 11:19:04 -1000

    FIX - adds missing python calibrations module

---
+++ python/gaps-online/gaps_online/tof/calibrations.py
@@ -0,0 +1,120 @@
+import gaps_tof as gt
+import pylab as p
+import numpy as np
+import dashi as d
+d.visual()
+
+try:
+    import charmingbeauty as cb
+    FIGSIZE=cb.layout.FIGSIZE_A4  
+except ImportError as e:
+    print(f"Can't find charmingbeauty for nice looking plots! {e}")
+    GOLDEN_RATIO = (1 + np.sqrt(5))/2.
+    WIDTH_A4 = 5.78851 # inch
+    FIGSIZE_A4_LANDSCAPE = (WIDTH_A4, WIDTH_A4/GOLDEN_RATIO)
+    FIGSIZE_A4_LANDSCAPE_HALF_HEIGHT = (WIDTH_A4, (WIDTH_A4/(2*GOLDEN_RATIO)))
+    FIGSIZE_A4 = (WIDTH_A4, (WIDTH_A4/GOLDEN_RATIO) + WIDTH_A4)
+    FIGSIZE=FIGSIZE_A4
+
+def _plot_constants(rb_id, data,var='offset', bins=20):
+    fig, axes = \
+      p.subplots(5, 1, sharex=True, figsize=cb.layout.FIGSIZE_A4)# layout='constrained', sharex=True)
+    fig.subplots_adjust(hspace=0)
+    ylabel = 'counts'
+    xpos   = 0.75
+    fig.text(-0.05, 0.85, ylabel,\
+             rotation=90,\
+             transform=fig.transFigure,\
+             size=14)
+    fig.text(0.75, 0.9,\
+            f'RB{rb_id:02}',\
+             transform=fig.transFigure,\
+             size=18) 
+    for k in axes:
+        k.spines['top'].set_visible(True)
+        k.spines['right'].set_visible(True)
+        k.grid(True)
+    match var:
+        case 'offset':
+            xlabel = 'V Offsets'
+            fig.text(xpos, 0.05,\
+                     xlabel,\
+                     transform=fig.transFigure,\
+                     size=14)
+            for ch in range(9):
+                _const_plotter(axes, ch, data, bins=bins)
+        case 'inc':
+            xlabel = 'V Incs'
+            fig.text(xpos, 0.05,\
+                     xlabel,\
+                     transform=fig.transFigure,\
+                     size=14)
+            for ch in range(9):
+                _const_plotter(axes, ch, data, bins=bins)
+        case 'dip':
+            xlabel = 'V Dips'
+            fig.text(xpos, 0.05,\
+                     xlabel,\
+                     transform=fig.transFigure,\
+                     size=14)
+            for ch in range(9):
+                _const_plotter(axes, ch, data, bins=bins)
+        case 'tbin':
+            xlabel = 'T Bins'
+            fig.text(xpos, 0.05,\
+                     xlabel,\
+                     transform=fig.transFigure,\
+                     size=14)
+            for ch in range(9):
+                _const_plotter(axes, ch, data, bins=bins)
+
+def _const_plotter(axes, ch, data, bins=20):
+
+    if ch == 9:
+        ax_j  = 4
+        color = 'k'
+        alpha = 1.0
+        lw    = 1.5
+        fill  = False
+    elif ch % 2 == 0:
+        ax_j  = int ((ch - 2) / 2)
+        color = 'tab:red'
+        alpha = 0.9
+        lw    = 0.9
+        fill  = False
+    else:
+        ax_j  = int ((ch - 1) / 2)
+        color = 'tab:blue'
+        alpha = 0.9
+        lw    = 0.9
+        fill  = True
+    p.sca(axes[ax_j])
+    h = d.factory.hist1d(data[ch], bins)
+    h.line(color  = color,\
+           alpha  = alpha,\
+           filled = fill,\
+           lw     = lw)
+
+def plot_offsets(self, bins=20):
+    data = self.v_offsets
+    return _plot_constants(self.rb_id,data,var='offset', bins=bins)
+
+def plot_dips(self, bins=20):
+    data = self.v_dips
+    return _plot_constants(self.rb_id,data,var='inc', bins=bins)
+
+def plot_incs(self, bins=20):
+    data = self.v_incs
+    return _plot_constants(self.rb_id,data,var='dip', bins=bins)
+
+def plot_tbins(self, bins=20):
+    data = self.t_bin
+    return _plot_constants(self.rb_id,data,var='tbin', bins=bins)
+
+# monkey patch the C++ API RBEvent
+gt.RBCalibration.plot_offsets = plot_offsets
+gt.RBCalibration.plot_dips    = plot_dips
+gt.RBCalibration.plot_incs    = plot_incs
+gt.RBCalibration.plot_tbins   = plot_tbins
+RBCalibration = gt.RBCalibration
+
