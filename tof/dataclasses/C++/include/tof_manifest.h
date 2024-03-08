465a619fa27fd59f0084f3f7108eb840e4cd5db3
Achim Stoessl <achim.stoessl@gmail.com>
2024-03-08 11:04:24 -1000

    WIP - started to work on cxx bindings to read gaps-db

--- tof/dataclasses/C++/include/tof_manifest.h
+++ tof/dataclasses/C++/include/tof_manifest.h
@@ -1,66 +1,23 @@
-/**))))))*************************************************
+/***************************************************
+ * C++ bindings to access the GAPS-DB 
  *
- * C++ bindings for tof manifest components. These are
- *
- *  - RBs
- *  - LTBs
- *  - Paddles
- *  
- *  for the detailed description of the database layout, 
- *  see the gaps-db project in the same repository
  *
  */
 
 #ifndef TOFMANIFEST_H_INCLUDED
 #define TOFMANIFEST_H_INCLUDED
 
-struct LTB {
-    u16 ltb_id         ; 
-    u16 ltb_dsi        ; 
-    u16 ltb_j          ; 
-    u16 ltb_ch1_rb     ; 
-    u16 ltb_ch2_rb     ; 
-    u16 ltb_ch3_rb     ; 
-    u16 ltb_ch4_rb     ; 
-    u16 ltb_ch5_rb     ; 
-    u16 ltb_ch6_rb     ; 
-    u16 ltb_ch7_rb     ; 
-    u16 ltb_ch8_rb     ; 
-    u16 ltb_ch9_rb     ; 
-    u16 ltb_ch10_rb    ; 
-    u16 ltb_ch11_rb    ; 
-    u16 ltb_ch12_rb    ; 
-    u16 ltb_ch13_rb    ; 
-    u16 ltb_ch14_rb    ; 
-    u16 ltb_ch15_rb    ; 
-    u16 ltb_ch16_rb    ; 
-    u16 ltb_ch17_rb    ; 
-    u16 ltb_ch18_rb    ; 
-    u16 ltb_ch19_rb    ; 
-    u16 ltb_ch20_rb    ; 
-    u16 ltb_ch1_rb_ch  ; 
-    u16 ltb_ch2_rb_ch  ; 
-    u16 ltb_ch3_rb_ch  ; 
-    u16 ltb_ch4_rb_ch  ; 
-    u16 ltb_ch5_rb_ch  ; 
-    u16 ltb_ch6_rb_ch  ; 
-    u16 ltb_ch7_rb_ch  ; 
-    u16 ltb_ch8_rb_ch  ; 
-    u16 ltb_ch9_rb_ch  ; 
-    u16 ltb_ch10_rb_ch ; 
-    u16 ltb_ch11_rb_ch ; 
-    u16 ltb_ch12_rb_ch ; 
-    u16 ltb_ch13_rb_ch ; 
-    u16 ltb_ch14_rb_ch ; 
-    u16 ltb_ch15_rb_ch ; 
-    u16 ltb_ch16_rb_ch ; 
-    u16 ltb_ch17_rb_ch ; 
-    u16 ltb_ch18_rb_ch ; 
-    u16 ltb_ch19_rb_ch ; 
-    u16 ltb_ch20_rb_ch ; 
+struct Paddle {
+  u16 paddle_id;
+  u64 volume_id;
+  std::string pos_in_paddle;
+  f32 height;
+  f32 width;
+  f32 length;
+  std::string unit;
+  f32 global_pos_x_l0;
+  f32 global_pos_y_l0;
+  f32 global_pos_z_l0;
 }
 
-
-
 #endif
-
