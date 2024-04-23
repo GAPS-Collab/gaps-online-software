f649974fc5afb0721715e68e99c9480de58587b0
Achim Stoessl <achim.stoessl@gmail.com>
2024-04-22 17:41:02 -1000

    REFACTOR - file rename flight_comms -> global_data_sink
    * Rename the file of the name of the thread

--- tof/liftof/liftof-cc/src/threads.rs
+++ tof/liftof/liftof-cc/src/threads.rs
@@ -6,6 +6,6 @@ pub mod command_dispatcher;
 
 pub use self::event_builder::event_builder;
 pub use self::readoutboard_comm::readoutboard_communicator;
-pub use self::flight_comms::global_data_sink;
+pub use self::global_data_sink::global_data_sink;
 pub use self::monitoring::monitor_cpu;
 pub use self::command_dispatcher::command_dispatcher;
