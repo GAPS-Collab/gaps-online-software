5a11163f67b03ecc09287ad996ff3be441a9c1f0
Achim Stoessl <achim.stoessl@gmail.com>
2024-05-06 13:14:22 -1000

    REFACTOR - updates benchmarks

--- tof/dataclasses/rust/tof-dataclasses/benches/bench.rs
+++ tof/dataclasses/rust/tof-dataclasses/benches/bench.rs
@@ -1,116 +1,144 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{
+    //use tof_dataclasses::events::TofEvent;black_box, 
+    criterion_group,
+    criterion_main,
+    Criterion
+};
 
 #[macro_use]
 extern crate log;
 use std::path::Path;
+
+//use tempfile::NamedTempFile;
+
 use tof_dataclasses::events::{
-    RBEventMemoryView,
     RBEvent
 };
-use tof_dataclasses::packets::TofPacket;
+//use tof_dataclasses::packets::TofPacket;
 use tof_dataclasses::io::{
     read_file,
-    RobinReader,
-    RBEventMemoryStreamer
+    RBEventMemoryStreamer,
+    TofPacketWriter, 
+    TofPacketReader,
+    FileType
 };
 
-use tof_dataclasses::serialization::Serialization;
+use tof_dataclasses::events::TofEvent;
+use tof_dataclasses::packets::TofPacket;
+
+use tof_dataclasses::serialization::{
+    Packable
+};
 use tof_dataclasses::FromRandom;
 
+// setup tests
+
+fn write_testfile(nevents : usize) -> String {
+  let fname = String::from("/tmp/");
+  //let fname_c = fname.clone();
+  let mut writer = TofPacketWriter::new(fname.clone(), FileType::RunFile(1));
+  for _ in 0..nevents {
+    let tp = TofPacket::from_random();
+    writer.add_tof_packet(&tp);
+  }
+  writer.file_name.clone()
+}
+
+
+fn tpreader_read() {
+  let fname = write_testfile(1000);
+  //let fname = data.path().to_str().map(|s| s.to_owned()).unwrap();
+  let mut reader = TofPacketReader::new(fname);
+  for tp in reader.next() {
+    //foo
+  }
+}
 
 // FIXME - remember to measure throuput as well
 // group.throughput(Throughput::Bytes(bytes.len() as u64));
 
-fn bench_tofpacket_serialize_helper() {
-  let mut n_iter = 0u32;
-  let ev = RBEvent::from_random();
-  loop {
-    let tp = TofPacket::from(&ev);
-    let stream = tp.to_bytestream();
-    match TofPacket::from_bytestream(&stream, &mut 0) {
-      Err(err) => {
-        error!("Can't unpack stream! {err}");
-      },
-      Ok(_) => (),
+fn pack_tofevent_worst() {
+  for _ in 0..100 {
+    let mut te   = TofEvent::new();
+    for _ in 0..40 {
+      let mut rbev = RBEvent::new();
+      for k in 0..8 {
+        let data = vec![0u16;1024];
+        rbev.adc[k] = data;
+      }
+      te.rb_events.push(rbev);
     }
-    n_iter += 1;
-    if n_iter == 100 {
-      break;
-    }
+    let pack = te.pack();
+    let _ : TofEvent = pack.unpack().unwrap();
   }
 }
 
-
-fn bench_multitofpacket_serialize_helper() {
-  let mut n_iter = 0u32;
-  let ev = RBEvent::from_random();
-  //let mut packets = Vec::<TofPacket>::with_capacity(1000);
-  let mut packets = Vec::<TofPacket>::new();
-  loop {
-    let tp = TofPacket::from(&ev);
-    packets.push(tp);
-    //let stream = tp.to_bytestream();
-    //match TofPacket::from_bytestream(&stream, &mut 0) {
-    //  Err(err) => {
-    //    error!("Can't unpack stream!");
-    //  },
-    //  Ok(_) => (),
-    //}
-    n_iter += 1;
-    if n_iter == 100 {
-      break;
+fn pack_tofevent_best() {
+  for _ in 0..100 {
+    let mut te   = TofEvent::new();
+    for _ in 0..1 {
+      let mut rbev = RBEvent::new();
+      for k in 0..1 {
+        let data = vec![0u16;1024];
+        rbev.adc[k] = data;
+      }
+      te.rb_events.push(rbev);
     }
+    let pack = te.pack();
+    let _ : TofEvent = pack.unpack().unwrap();
   }
 }
 
-fn bench_multitofpacket_serialize(c: &mut Criterion) {
-  c.bench_function("multitofpacket_serialize", |b|
-                   b.iter(|| bench_multitofpacket_serialize_helper()));
+fn pack_tofevent_average() {
+  for _ in 0..100 {
+    let mut te   = TofEvent::new();
+    for _ in 0..4 {
+      let mut rbev = RBEvent::new();
+      for k in 0..2 {
+        let data = vec![0u16;1024];
+        rbev.adc[k] = data;
+      }
+      te.rb_events.push(rbev);
+    }
+    let pack = te.pack();
+    let _ : TofEvent = pack.unpack().unwrap();
+  }
 }
 
-fn bench_tofpacket_serialize(c: &mut Criterion) {
-  c.bench_function("tofpacket_serialize", |b|
-                   b.iter(|| bench_tofpacket_serialize_helper()));
+fn bench_pack_tofevent_worst(c: &mut Criterion) {
+  c.bench_function("pack_tofevent_worst", |b|
+                   b.iter(|| pack_tofevent_worst()));
+}
+
+fn bench_pack_tofevent_best(c: &mut Criterion) {
+  c.bench_function("pack_tofevent_best", |b|
+                   b.iter(|| pack_tofevent_best()));
+}
+
+fn bench_pack_tofevent_average(c: &mut Criterion) {
+  c.bench_function("pack_tofevent_average", |b|
+                   b.iter(|| pack_tofevent_average()));
+}
+
+fn pack_rbevent() {
+  let ev = RBEvent::from_random();
+  let _ : RBEvent = ev.pack().unpack().unwrap();
+}
+
+fn bench_tpreader_read(c: &mut Criterion) {
+  c.bench_function("tpreader_read", |b| 
+                   b.iter(|| tpreader_read()));
+}
+
+
+fn bench_pack_rbevent(c: &mut Criterion) {
+  c.bench_function("pack_rbevent", |b|
+                   b.iter(|| pack_rbevent()));
 }
 
 fn bench_read_file(c: &mut Criterion) {
   c.bench_function("read_file", |b|
                    b.iter(|| read_file(&Path::new("test-data/tof-rb01.robin")).unwrap()));
-}
-
-fn bench_rreader_read_helper() {
-  let mut reader = RobinReader::new(("test-data/tof-rb01.robin").to_string());
-  //reader.cache_all_events();
-  let mut nevents = 0usize;
-  loop {
-    match reader.next() {
-      None => {
-        break;
-      },
-      Some(ev) => {
-        nevents += 1;
-        let _ = TofPacket::from(&ev);
-        continue;
-      }
-    }
-  }
-  //println!("extracted {} events!", nevents);
-}
-
-fn bench_rbeventmemoryview_readfile_helper() {
-  let data = read_file(&Path::new("test-data/tof-rb01.robin")).unwrap();
-  let mut pos = 0usize;
-  let mut nevents = 0usize;
-  while data.len() - pos > 18530 {
-    let _ = RBEventMemoryView::from_bytestream(&data, &mut pos);
-    nevents += 1;
-  }
-  //println!("extracted {} events", nevents);
-}
-
-fn bench_rbeventmemoryview_readfile(c: &mut Criterion) {
-  c.bench_function("rreventmemoryview_readfile", |b|
-                   b.iter(|| bench_rbeventmemoryview_readfile_helper()));
 }
 
 fn bench_streamer_index_helper() {
@@ -177,68 +205,24 @@ fn bench_streamer_tofpacket(c: &mut Criterion) {
                    b.iter(|| bench_streamer_tofpacket_helper()));
 }
 
-fn bench_rbevent_serialization_helper() {
-  let ev = RBEvent::from_random();
-  let result = RBEvent::from_bytestream(&ev.to_bytestream(), &mut 0).unwrap();
-  let _  = TofPacket::from(&result);
-}
-
-fn bench_rbevent_serialization(c: &mut Criterion) {
-  c.bench_function("rbevent_serialization", |b|
-                   b.iter(|| bench_rbevent_serialization_helper()));
-}
 
 
-fn bench_rreader_read(c: &mut Criterion) {
-  c.bench_function("rreader_read", |b|
-                   b.iter(|| bench_rreader_read_helper()));
-}
-
-cfg_if::cfg_if! {
-  if #[cfg(feature = "random")] {
-    fn bench_rbmemoryview_from_random(c: &mut Criterion) {
-      c.bench_function("rbmemoryview_fromrandom", |b|
-                       b.iter(|| RBEventMemoryView::from_random()));
-    }
-
-
-    fn bench_rbmemoryview_serialization_circle_helper() {
-      let data   = RBEventMemoryView::from_random();
-      let stream = data.to_bytestream();
-      let _      = RBEventMemoryView::from_bytestream(&stream, &mut 0);
-    }
-
-    fn bench_rbmemoryview_serialization_circle(c: &mut Criterion) {
-      c.bench_function("rbinbary_dump_serialization_circle", |b|
-        b.iter(|| bench_rbmemoryview_serialization_circle_helper()) 
-      );
-    }
-
-    fn bench_rbmemoryview_serialization_circle_norandom(c: &mut Criterion) {
-      c.bench_function("rbinbary_dump_serialization_circle_norandom", |b|
-        b.iter(|| {
-          let data = RBEventMemoryView::new();
-          let _ = RBEventMemoryView::from_bytestream(&data.to_bytestream(), &mut 0);
-        }) 
-      );
-    }
-  }
-}
 
 criterion_group!(name = benches;
                  config = Criterion::default().sample_size(10);
-                 targets = bench_read_file, bench_rreader_read, bench_rbeventmemoryview_readfile, bench_streamer_tofpacket, bench_streamer_index, bench_streamer_next,
-                 );
+                 targets = bench_read_file,
+                           bench_streamer_tofpacket,
+                           bench_streamer_index,
+                           bench_streamer_next,
+                           bench_tpreader_read);
 
 cfg_if::cfg_if! {
   if #[cfg(feature = "random")] {
     criterion_group!(benches_random,
-                     bench_multitofpacket_serialize,
-                     bench_tofpacket_serialize,
-                     bench_rbmemoryview_from_random,
-                     bench_rbmemoryview_serialization_circle,
-                     bench_rbmemoryview_serialization_circle_norandom,
-                     bench_rbevent_serialization
+                     bench_pack_tofevent_worst,
+                     bench_pack_tofevent_best,
+                     bench_pack_tofevent_average,
+                     bench_pack_rbevent,
                      );
     criterion_main!(benches, benches_random);
   } else {
