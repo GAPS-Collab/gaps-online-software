1e2ad8c7c5a597896eb966bac446de4dbe841f3f
Achim Stoessl <achim.stoessl@gmail.com>
2024-07-22 05:59:27 -1000

    FEATUREp/WIP - prepare liftof-cc for better runid management
    * Ultimately, we want automatic run id assignment
    * This allows to check for all run ids in a directory and get the
      next in order

--- tof/liftof/liftof-cc/src/lib.rs
+++ tof/liftof/liftof-cc/src/lib.rs
@@ -5,7 +5,7 @@ extern crate colored;
 extern crate crossbeam_channel;
 
 extern crate zmq;
-extern crate openssh;
+//extern crate openssh;
 extern crate comfy_table;
 extern crate tof_dataclasses;
 #[cfg(features="tof-ctrl")]
@@ -15,8 +15,50 @@ extern crate liftof_lib;
 pub mod constants;
 pub mod threads;
 
-use openssh::{Session, KnownHosts};
+use std::fs;
+use std::path::PathBuf;
 
+/// Prepare a new folder with the run id
+///
+/// This will assign a run id based on the 
+/// run ids in that folder
+///
+/// TODO - connect it to the run database
+///
+/// # Arguments:
+///
+/// * data_path : The global path on the inflight 
+///               tof computer where to store data.
+pub fn prepare_run(data_path : String) -> Option<u32> {
+  let stream_files_path = PathBuf::from(data_path);
+  let paths = fs::read_dir(stream_files_path.clone()).unwrap();
+  let mut used_runids = Vec::<u32>::new();
+  for path in paths {
+    // this is hideous, I am so sorry. May the rust gods have mercy on my soul...
+    match format!("{}",path.as_ref().unwrap().path().iter().last().unwrap().to_str().unwrap()).parse::<u32>() {
+      Ok(this_run_id) => {
+        debug!("Extracted run id {}", this_run_id);
+        used_runids.push(this_run_id);
+      },
+      Err(err)        => {
+        warn!("Can not get runid from {}! {}", path.unwrap().path().display(), err);
+      }
+    }
+  }
+  let mut max_run_id = 0u32;
+  match used_runids.iter().max() {
+    None => (),
+    Some(_r) => {
+      max_run_id = *_r;
+    }
+  }
+  println!("=> Found {} used run ids in {}. Largest run id is {}",used_runids.len(), stream_files_path.display(), max_run_id);
+  if max_run_id == 0 {
+    return None;
+  } else {
+    return Some(max_run_id + 1);
+  }
+}
 
 
 #[deprecated(since="0.10.2", note="Unclear purpose")]
