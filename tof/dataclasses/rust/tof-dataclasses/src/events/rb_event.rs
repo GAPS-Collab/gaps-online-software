e2f3227ae514c5a18de5404ee2c49412a4a19b0d
Achim Stoessl <achim.stoessl@gmail.com>
2024-10-23 23:37:20 -1000

    FEATURE - allow to switch between fpga_temp and drs_deadtime

--- tof/dataclasses/rust/tof-dataclasses/src/events/rb_event.rs
+++ tof/dataclasses/rust/tof-dataclasses/src/events/rb_event.rs
@@ -753,63 +753,99 @@ impl From<&TofPacket> for RBEvent {
 /// Contains information about event id, timestamps,
 /// etc.
 #[derive(Debug, Clone, PartialEq)]
-pub struct RBEventHeader {
-  pub rb_id                : u8   , 
-  pub event_id             : u32  , 
-  pub status_byte          : u8   ,
-  // FIXME - channel mask still has space
-  // for the status_bytes, since it only
-  // uses 9bits
-  pub channel_mask         : u16  , 
+pub struct RBEventHeader { 
+  /// Readoutboard ID - should be in the range 1-50
+  /// not consecutive, there are some missing.
+  /// In general, we have 40 boards
+  pub rb_id                : u8   ,
+  /// The event ID as sent from the MTB or self-generated
+  /// if not latched to the MTB
+  pub event_id             : u32  ,
+  /// The DRS stop cell. This is vital information which is
+  /// needed for the calibration
   pub stop_cell            : u16  , 
   // we change this by keeping the byte
   // order the same to accomodate the sine 
   // values
-  pub ch9_amp              : u16,
-  pub ch9_freq             : u16,
-  pub ch9_phase            : u32,
-  //pub crc32              : u32  , 
-  //pub dtap0              : u16  , 
-  //pub drs4_temp          : u16  , 
-  pub fpga_temp            : u16  , 
-  pub drs_deadtime         : u16,
-  pub timestamp32          : u32,
-  pub timestamp16          : u16,
-  // channels (0-8)
-  //pub channels             : Vec<u8>,
-  //// fields which don't get serialized
-  //pub nwords               : usize,
-  //pub channel_packet_len   : usize,
-  //pub channel_packet_start : usize,
-  //pub channel_packet_ids   : Vec<u8>,
+  pub ch9_amp               : u16,
+  pub ch9_freq              : u16,
+  pub ch9_phase             : u32,
+  /// The adc value for the temperature
+  /// of the FPGA
+  pub fpga_temp             : u16, 
+  /// DRS deadtime as read out from the 
+  /// register
+  pub drs_deadtime          : u16,
+  pub timestamp32           : u32,
+  pub timestamp16           : u16,
+  /// Store the drs_deadtime instead 
+  /// of the fpga temperature
+  pub deadtime_instead_temp : bool,
+  /// The status byte contains information about lsos of lock
+  /// and event fragments and needs to be decoded
+  status_byte              : u8   ,
+  /// The channel mask is 9bit for the 9 channels.
+  /// This leaves 7 bits of space so we actually 
+  /// hijack that for the version information 
+  /// 
+  /// Bit 15 will be set 1 in case we are sending
+  /// the DRS_DEADTIME instead of FPGA TEMP
+  ///
+  /// FIXME - make this proper and use ProtocolVersion 
+  ///         instead
+  channel_mask             : u16  , 
 }
 
 impl RBEventHeader {
 
   pub fn new() -> Self {
     Self {
-      rb_id                : 0,  
-      status_byte          : 0, 
-      event_id             : 0,  
-      channel_mask         : 0 ,  
-      stop_cell            : 0 ,  
-      ch9_amp              : 0 ,
-      ch9_freq             : 0 ,
-      ch9_phase            : 0 ,
-      //crc32                : 0 ,  
-      //dtap0                : 0 ,  
-      //drs4_temp            : 0 ,  
-      fpga_temp            : 0,  
-      drs_deadtime         : 0,
-      timestamp32          : 0,
-      timestamp16          : 0,
-      //channels             : Vec::<u8>::with_capacity(9),
-      //// fields that won't get serialized
-      //nwords               : 0,
-      //channel_packet_len   : 0,
-      //channel_packet_start : 0,
-      //channel_packet_ids   : Vec::<u8>::with_capacity(9),
+      rb_id                 : 0,  
+      status_byte           : 0, 
+      event_id              : 0,  
+      channel_mask          : 0,  
+      stop_cell             : 0,  
+      ch9_amp               : 0,
+      ch9_freq              : 0,
+      ch9_phase             : 0,
+      fpga_temp             : 0,  
+      drs_deadtime          : 0,
+      timestamp32           : 0,
+      timestamp16           : 0,
+      deadtime_instead_temp : false,
     }
+  }
+
+  /// Set the channel mask with the 9bit number
+  ///
+  /// Set bit 15 to either 1 or 0 depending on
+  /// deadtime_instead_temp
+  pub fn set_channel_mask(&mut self, channel_mask : u16) {
+    if self.deadtime_instead_temp {
+      self.channel_mask = 2u16.pow(15) | channel_mask;
+    } else {
+      self.channel_mask = channel_mask;
+    }
+  }
+
+  /// Just return the channel mask and strip of 
+  /// the part which contains the information about
+  /// drs deadtime or fpga temp
+  pub fn get_channel_mask(&self) -> u16 {
+    self.channel_mask & 0x1ff 
+  }
+
+  /// Get the channel mask from a bytestream.
+  /// 
+  /// This takes into acount that bit 15 is 
+  /// used to convey information about if we
+  /// stored the drs temperature or deadtime
+  pub fn parse_channel_mask(ch_mask : u16) -> (bool, u16) {
+    let channel_mask          : u16;
+    let deadtime_instead_temp : bool 
+      = ch_mask >> 15 == 1;
+    channel_mask = ch_mask & 0x1ff;
+    (deadtime_instead_temp, channel_mask)
   }
 
   pub fn set_sine_fit(&mut self, input : (f32, f32, f32)) {
@@ -877,6 +913,7 @@ impl RBEventHeader {
   pub fn is_locked_last_sec(&self) -> bool {
     !self.lost_lock_last_sec()
   }
+  
   /// extract lock, drs busy and fpga temp from status field
   pub fn parse_status(&mut self, status_bytes : u16) {
     // status byte is only 4bit really
@@ -941,8 +978,11 @@ impl fmt::Display for RBEventHeader {
     //repr += &("\n  DRS4 temp [C]    ".to_owned() + &self.drs4_temp.to_string());  
     repr += &sine_field;
     //repr += &("\n  FPGA temp [\u{00B0}C]    ".to_owned() + &self.get_fpga_temp().to_string()); 
-    repr += &(format!("\n  FPGA T [\u{00B0}C]    : {:.2}", self.get_fpga_temp()));
-    repr += &(format!("\n  DRS deadtime          : {:.2}", self.drs_deadtime));
+    if self.deadtime_instead_temp {
+      repr += &(format!("\n  DRS deadtime          : {:.2}", self.drs_deadtime));
+    } else {
+      repr += &(format!("\n  FPGA T [\u{00B0}C]    : {:.2}", self.get_fpga_temp()));
+    }
     repr += &(format!("\n  timestamp32      {}", self.timestamp32            )); 
     repr += &(format!("\n  timestamp16      {}", self.timestamp16            )); 
     repr += &(format!("\n   |-> timestamp48 {}", self.get_timestamp48()      )); 
@@ -984,20 +1024,25 @@ impl Serialization for RBEventHeader {
     -> Result<Self, SerializationError> {
     let mut header  = Self::new();
     Self::verify_fixed(stream, pos)?;
-    header.rb_id               = parse_u8(stream  , pos);  
-    header.event_id            = parse_u32(stream , pos);  
-    header.channel_mask        = parse_u16(stream  , pos);   
-    header.status_byte         = parse_u8(stream, pos);
-    header.stop_cell           = parse_u16(stream , pos);  
-    header.ch9_amp             = parse_u16(stream, pos);
-    header.ch9_freq            = parse_u16(stream, pos);
-    header.ch9_phase           = parse_u32(stream, pos);
-    //VERSION > 0.10.5 - we replace DRS temperature with 
-    //DRS DEADTIME
-    //header.fpga_temp           = parse_u16(stream , pos);  
-    header.drs_deadtime        = parse_u16(stream, pos);
-    header.timestamp32         = parse_u32(stream, pos);
-    header.timestamp16         = parse_u16(stream, pos);
+    header.rb_id                 = parse_u8 (stream, pos);  
+    header.event_id              = parse_u32(stream, pos);  
+    let ch_mask                  = parse_u16(stream, pos);
+    let (deadtime_instead_temp, channel_mask)  
+      = Self::parse_channel_mask(ch_mask);
+    header.deadtime_instead_temp = deadtime_instead_temp;
+    header.set_channel_mask(channel_mask);
+    header.status_byte         = parse_u8 (stream, pos);
+    header.stop_cell             = parse_u16(stream, pos);  
+    header.ch9_amp               = parse_u16(stream, pos);
+    header.ch9_freq              = parse_u16(stream, pos);
+    header.ch9_phase             = parse_u32(stream, pos);
+    if deadtime_instead_temp {
+      header.drs_deadtime        = parse_u16(stream, pos);
+    } else {
+      header.fpga_temp           = parse_u16(stream, pos);
+    }
+    header.timestamp32           = parse_u32(stream, pos);
+    header.timestamp16           = parse_u16(stream, pos);
     *pos += 2; // account for tail earlier 
     Ok(header) 
   }
@@ -1008,14 +1053,18 @@ impl Serialization for RBEventHeader {
     stream.extend_from_slice(&Self::HEAD.to_le_bytes());
     stream.extend_from_slice(&self.rb_id             .to_le_bytes());
     stream.extend_from_slice(&self.event_id          .to_le_bytes());
-    stream.extend_from_slice(&self.channel_mask      .to_le_bytes());
+    let ch_mask = ((self.deadtime_instead_temp as u16) << 15) | self.get_channel_mask();
+    stream.extend_from_slice(&ch_mask                .to_le_bytes());
     stream.extend_from_slice(&self.status_byte       .to_le_bytes());
     stream.extend_from_slice(&self.stop_cell         .to_le_bytes());
     stream.extend_from_slice(&self.ch9_amp           .to_le_bytes());
     stream.extend_from_slice(&self.ch9_freq          .to_le_bytes());
     stream.extend_from_slice(&self.ch9_phase         .to_le_bytes());
-    //stream.extend_from_slice(&self.fpga_temp         .to_le_bytes());
-    stream.extend_from_slice(&self.drs_deadtime      .to_le_bytes());
+    if self.deadtime_instead_temp {
+      stream.extend_from_slice(&self.drs_deadtime    .to_le_bytes());
+    } else {
+      stream.extend_from_slice(&self.fpga_temp       .to_le_bytes());
+    }
     stream.extend_from_slice(&self.timestamp32       .to_le_bytes());
     stream.extend_from_slice(&self.timestamp16       .to_le_bytes());
     stream.extend_from_slice(&RBEventHeader::TAIL.to_le_bytes());
@@ -1030,19 +1079,24 @@ impl FromRandom for RBEventHeader {
     let mut header = RBEventHeader::new();
     let mut rng = rand::thread_rng();
 
-    header.rb_id                = rng.gen::<u8>();    
-    header.event_id             = rng.gen::<u32>();   
-    header.channel_mask         = rng.gen::<u16>();    
-    header.status_byte          = rng.gen::<u8>();    
-    header.stop_cell            = rng.gen::<u16>();   
-    header.ch9_amp              = rng.gen::<u16>();
-    header.ch9_freq             = rng.gen::<u16>();
-    header.ch9_phase            = rng.gen::<u32>();
-    // Has been changed in V 0.10.5
-    //header.fpga_temp            = rng.gen::<u16>();  
-    header.drs_deadtime         = rng.gen::<u16>();
-    header.timestamp32          = rng.gen::<u32>();
-    header.timestamp16          = rng.gen::<u16>();
+    header.rb_id                 = rng.gen::<u8>();    
+    header.event_id              = rng.gen::<u32>();   
+    header.status_byte           = rng.gen::<u8>();    
+    header.stop_cell             = rng.gen::<u16>();   
+    header.ch9_amp               = rng.gen::<u16>();
+    header.ch9_freq              = rng.gen::<u16>();
+    header.ch9_phase             = rng.gen::<u32>();
+    header.deadtime_instead_temp = rng.gen::<bool>();
+    if header.deadtime_instead_temp {
+      header.drs_deadtime          = rng.gen::<u16>();
+    } else {
+      header.fpga_temp             = rng.gen::<u16>();  
+    }
+    // make sure the generated channel mask is valid!
+    let ch_mask                  = rng.gen::<u16>() & 0x1ff;
+    header.set_channel_mask(ch_mask);
+    header.timestamp32           = rng.gen::<u32>();
+    header.timestamp16           = rng.gen::<u16>();
     header
   }
 }
@@ -1204,19 +1258,24 @@ mod test_rbevents {
       RBEvent,
       RBEventHeader,
   };
+  
   #[test]
   fn serialization_rbeventheader() {
-    let mut pos = 0usize;
-    let head = RBEventHeader::from_random();
-    let stream = head.to_bytestream();
-    assert_eq!(stream.len(), RBEventHeader::SIZE);
-    let test = RBEventHeader::from_bytestream(&stream, &mut pos).unwrap();
-    assert_eq!(pos, RBEventHeader::SIZE);
-    assert_eq!(head, test);
-    assert_eq!(head.lost_lock()         , test.lost_lock());
-    assert_eq!(head.lost_lock_last_sec(), test.lost_lock_last_sec());
-    assert_eq!(head.drs_lost_trigger()  , test.drs_lost_trigger());
-
+    for _ in 0..100 {
+      let mut pos = 0usize;
+      let head = RBEventHeader::from_random();
+      println!("{}",  head);
+      let stream = head.to_bytestream();
+      assert_eq!(stream.len(), RBEventHeader::SIZE);
+      let test = RBEventHeader::from_bytestream(&stream, &mut pos).unwrap();
+      println!("{}", test);
+      assert_eq!(pos, RBEventHeader::SIZE);
+      assert_eq!(head, test);
+      assert_eq!(head.lost_lock()         , test.lost_lock());
+      assert_eq!(head.lost_lock_last_sec(), test.lost_lock_last_sec());
+      assert_eq!(head.drs_lost_trigger()  , test.drs_lost_trigger());
+      assert_eq!(head, test);
+    }
   }
   
   #[test]
