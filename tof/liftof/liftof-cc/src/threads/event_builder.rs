dc162cfdd2ce96c9d425175fb28d9f2aa6dad89b
Grace Tytus <gtytus@hawaii.edu>
2024-11-14 13:41:03 -1000

    FIX -- typo; hopefully data mangled events show up properly now

--- tof/liftof/liftof-cc/src/threads/event_builder.rs
+++ tof/liftof/liftof-cc/src/threads/event_builder.rs
@@ -608,8 +608,8 @@ pub fn event_builder (m_trig_ev      : &Receiver<MasterTriggerEvent>,
             // (optionally) produced tof event summary if the 
             // event has isuses
             n_rbe_per_te  += ev_to_send.rb_events.len();
-            heartbeat.data_mangled_ev = 69;
-            let _ev_satus  = ev_to_send.mt_event.event_status;
+            heartbeat.data_mangled_ev = 0;
+            let _ev_status  = ev_to_send.mt_event.event_status;
             for ev in &ev_to_send.rb_events {
               if ev.status == EventStatus::CellSyncErrors || ev.status == EventStatus::ChnSyncErrors {
                 ev_to_send.mt_event.event_status = EventStatus::AnyDataMangling {
