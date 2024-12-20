bf240f258163056f7a65c304065bd29ead5aa82b
Achim Stoessl <stoessl@hawaii.edu>
2024-12-20 18:36:54 +1300

    UPDATE - updated paddle gains (spreadhseet)

--- tof/resources/scripts/convert-pavolt-spreadsheat.py
+++ tof/resources/scripts/convert-pavolt-spreadsheat.py
@@ -17,26 +17,32 @@ if __name__ == '__main__':
     
     import sys
 
-    volts = pl.read_excel(sys.argv[1])
+    try:
+        volts = pl.read_excel(sys.argv[1])
+    except Exception as e:
+        volts = pl.read_csv(sys.argv[1])
+
+    # paddle #,end (A/B),raise/lower,RAT,PB CH,original HV,new HV        
     rats = dict()
     for k in range(1,21):
         rats[f'RAT{k:02}'] = [58.0]*16
     #print (rats)
     
-    for  row in volts.rows():
-        pid     = row[1]
+    for  row in volts.rows()[1:]:
+        print(row)
+        pid     = row[0]
         pa_volt = row[4]
-        if pid is not None:
-            last_pid = pid
-        if pid is None:
-            pid = last_pid
+        #if pid is not None:
+        #    last_pid = pid
+        #if pid is None:
+        #    pid = last_pid
         try:
-            rat, pb_ch = get_pbch_for_pid(pid, row[2])
+            rat, pb_ch = get_pbch_for_pid(pid, row[1])
         except Exception as e:
             print (e)
             continue
         key = f'RAT{rat:02}'
-        rats[key][pb_ch - 1] = pa_volt 
+        rats[key][pb_ch - 1] = float(pa_volt) 
     
     for k in rats:
         print (f'{k}={rats[k]}')
