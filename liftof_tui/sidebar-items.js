bbc4573d0e6e6dce0e375668350f5b485729131b
achim1 <achim1@users.noreply.github.com>
2025-01-06 09:34:49 +0000

    Deploying to gh-pages from  @ 2029a7d570ed75950a55478002e9785a1af4b843 🚀

--- liftof_tui/sidebar-items.js
+++ liftof_tui/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["packet_distributor","socket_wrap_tofstream","telly_packet_counter"],"mod":["colors","layout","menu","tabs","widgets"]};
+window.SIDEBAR_ITEMS = {"fn":["packet_distributor","render_logs","socket_wrap_telemetry","socket_wrap_tofstream","telly_packet_counter"],"mod":["colors","layout","menu","tabs","widgets"]};
