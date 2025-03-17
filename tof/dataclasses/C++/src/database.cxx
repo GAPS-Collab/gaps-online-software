ce5103414742d998d9339268e383aac30c043aea
Achim Stoessl <stoessl@hawaii.edu>
2025-03-17 19:02:54 +1300

    FEATURE - normal, principal vector for paddles
    * updates the CXX implementation for gaps db to
      include normal and principal vector as well as
      coordinates for each side indiviudally

--- tof/dataclasses/C++/src/database.cxx
+++ tof/dataclasses/C++/src/database.cxx
@@ -1,4 +1,5 @@
 #ifdef BUILD_CXXDB
+#include <cmath>
 #include <format>
 #include <iostream>
 #include "database.h"
@@ -28,12 +29,18 @@ std::string Gaps::TofPaddle::to_string() const {
   repr += std::format("\n  height              : {} ", height           );         
   repr += std::format("\n  width               : {} ", width            );         
   repr += std::format("\n  length              : {} ", length           );         
+  repr += std::format("\n  normal_x            : {} ", normal_x         );         
+  repr += std::format("\n  normal_y            : {} ", normal_y         );         
+  repr += std::format("\n  normal_z            : {} ", normal_z         );         
   repr += std::format("\n  global_pos_x_l0     : {} ", global_pos_x_l0  );         
   repr += std::format("\n  global_pos_y_l0     : {} ", global_pos_y_l0  );         
   repr += std::format("\n  global_pos_z_l0     : {} ", global_pos_z_l0  );         
   repr += std::format("\n  global_pos_x_l0_A   : {} ", global_pos_x_l0_A);          
   repr += std::format("\n  global_pos_y_l0_A   : {} ", global_pos_y_l0_A);          
   repr += std::format("\n  global_pos_z_l0_A   : {}>", global_pos_z_l0_A);         
+  repr += std::format("\n  global_pos_x_l0_B   : {} ", global_pos_x_l0_B);          
+  repr += std::format("\n  global_pos_y_l0_B   : {} ", global_pos_y_l0_B);          
+  repr += std::format("\n  global_pos_z_l0_B   : {}>", global_pos_z_l0_B);         
   return repr;
 }
 
@@ -60,12 +67,18 @@ std::map<u8, Gaps::TofPaddle> Gaps::get_tofpaddles(std::string dbname) {
       make_column("height"           , &Gaps::TofPaddle::height           ),         
       make_column("width"            , &Gaps::TofPaddle::width            ),         
       make_column("length"           , &Gaps::TofPaddle::length           ),         
+      make_column("normal_x"         , &Gaps::TofPaddle::normal_x         ),         
+      make_column("normal_y"         , &Gaps::TofPaddle::normal_y         ),         
+      make_column("normal_z"         , &Gaps::TofPaddle::normal_z         ),         
       make_column("global_pos_x_l0"  , &Gaps::TofPaddle::global_pos_x_l0  ),         
       make_column("global_pos_y_l0"  , &Gaps::TofPaddle::global_pos_y_l0  ),         
       make_column("global_pos_z_l0"  , &Gaps::TofPaddle::global_pos_z_l0  ),         
       make_column("global_pos_x_l0_A", &Gaps::TofPaddle::global_pos_x_l0_A),          
       make_column("global_pos_y_l0_A", &Gaps::TofPaddle::global_pos_y_l0_A),          
-      make_column("global_pos_z_l0_A", &Gaps::TofPaddle::global_pos_z_l0_A)));          
+      make_column("global_pos_z_l0_A", &Gaps::TofPaddle::global_pos_z_l0_A),          
+      make_column("global_pos_x_l0_B", &Gaps::TofPaddle::global_pos_x_l0_B),          
+      make_column("global_pos_y_l0_B", &Gaps::TofPaddle::global_pos_y_l0_B),          
+      make_column("global_pos_z_l0_B", &Gaps::TofPaddle::global_pos_z_l0_B)));          
   
   auto paddles = storage.get_all<Gaps::TofPaddle>();
   auto paddle_map = std::map<u8, Gaps::TofPaddle>();
@@ -73,6 +86,20 @@ std::map<u8, Gaps::TofPaddle> Gaps::get_tofpaddles(std::string dbname) {
     paddle_map.insert({p.paddle_id, p});
   }  
   return paddle_map;
+}
+
+auto Gaps::TofPaddle::get_principal() const -> Vec<f32> {
+  Vec<f32> pr(3,0);
+  pr[0] = global_pos_x_l0_A - global_pos_x_l0;
+  pr[1] = global_pos_y_l0_A - global_pos_y_l0;
+  pr[2] = global_pos_z_l0_A - global_pos_z_l0;
+  f32 length = std::sqrt((std::pow(pr[0],2) + std::pow(pr[1],2) + std::pow(pr[2],2)));
+  if (length > 0) {
+    pr = {pr[0]/length, pr[1]/length, pr[2]/length};
+  } else {
+    pr = {0,0,0};
+  }
+  return pr; 
 }
 
 std::ostream& operator<<(std::ostream& os, const Gaps::TofPaddle& tp) {
