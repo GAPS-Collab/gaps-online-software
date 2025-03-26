fb92616a8d45eaa6b584d2d4a5a9f22ecf072022
Achim Stoessl <achim.stoessl@gmail.com>
2025-03-26 09:37:59 -1000

    FIX - fix paddles once and for all?
    * new iteration of coordinates and paddle mapping spreadsheets from
      the tof team
    * should fix paddle 108 problem

--- gaps-db/resources/scripts/recreate_db.sh
+++ gaps-db/resources/scripts/recreate_db.sh
@@ -9,5 +9,5 @@ rm -rf tof-db/migrations
 python manage.py makemigrations
 python manage.py migrate
 cd ../resources/scripts/
-./create_tof_db_flight.py --coordinates ../master-spreadsheet/tof-paddle-orientations-clean.xlsx --volid-map ../master-spreadsheet/paddleid_vs_volid.json --cable-map ../master-spreadsheet/Jeff_paddle_cable.json --create-all-tables ../master-spreadsheet/GAPS_Channel_mapping.xlsx
+./create_tof_db_flight.py --coordinates ../master-spreadsheet/tof-paddle-orientations-clean.xlsx --volid-map ../master-spreadsheet/paddleid_vs_volid.json --cable-map ../master-spreadsheet/Jeff_paddle_cable.json --create-all-tables ../master-spreadsheet/GAPS_Channel_mapping_v2.1.xlsx
 ./create_trk_db_flight.py --coordinates ../master-spreadsheet/tracker-from-sd.json --pedestals ../tracker-calibration/pedestal_LDB_9December.txt 
