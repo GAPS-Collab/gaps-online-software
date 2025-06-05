3c0e2d3e9dda3fce714431bbf704041054137e42
Grace <gtytus@hawaii.edu>
2025-06-05 10:22:01 -1000

    FIXES: 1. remove dependency on charmingbeauty (i am sad to do this but it implicitly depends on latex which makes it difficult for people) 2. fixed access to volume id's from tree 3. changed color map from seismic -> gnuplot2 (easy to change though, just use the cm = .. in line 59)

--- analysis/run_vetting/reco_data_vetting.py
+++ analysis/run_vetting/reco_data_vetting.py
@@ -1,63 +1,65 @@
-#! /usr/bin/env python 
-
 import gaps_online as go
 import uproot as up
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import tqdm
-import charmingbeauty as cb
-import charmingbeauty.layout as lo
-
 from pathlib import Path
-
+import argparse
 import dashi as d
 
 matplotlib.use('agg')
 
-cb.set_style_present()
-d.visual()
+if __name__ == '__main__':
 
-files = Path('/data2/gaps/L2').glob('*.root')
-files = [k for k in files]
-vid_hid_map = go.db.get_vid_hid_map()
+    parser = argparse.ArgumentParser(description='Paddle occupancy graphic plot from root files (reco or MC)')
+    parser.add_argument('-dir', '--data_dir', type=str, default = '', help = 'A directory with .root files from MC or Reconstruction')
+    parser.add_argument('-id','--data_id', type=str, default ='_', help='the data id will go at the beginning of output files. the default is "_"')
+    args = parser.parse_args()
 
-pid_hist = d.factory.hist1d(np.array([]), bins=np.arange(0.5, 160.5, 1))
-occu = {k : 0 for k in range(1,161)}
-for f in tqdm.tqdm(files, total=len(files), desc='Creating plot...'):
-    f = up.open(f)
-    event_pids = []
-    vids = f.get('TreeRec').get('Rec').get('hitseries_/hitseries_.volume_id_').array()
-    for ev in vids:
-        pids = [vid_hid_map[k] for k in ev if k < 200000000]
-        event_pids.extend(pids)
-        for pid in pids:
-            occu[pid] += 1
-    pid_hist.fill(np.array(event_pids))
+    #cb.set_style_present()
+    d.visual()
 
-# normalize the occupancy
-max_occu = 0
-for k in occu:
-    if occu[k] > max_occu:
-        max_occu = occu[k]
-for k in occu:
-    occu[k] = occu[k]/max_occu
-    if occu[k] == 0:
-        occu[k] = np.nan
+    files = Path(f'{args.data_dir}').glob('*.root')
+    files = [k for k in files]
+    vid_hid_map = go.db.get_vid_hid_map()
 
-# plot paddle occupancy
-fig = plt.figure(figsize=lo.FIGSIZE_A4_LANDSCAPE_HALF_HEIGHT)
-ax  = fig.gca()
-pid_hist.line(filled=True, alpha=0.4, color='tab:blue')
-cb.visual.adjust_minor_ticks(ax)
-ax.set_ylim(bottom=0)
-fig.savefig('pid-hist-reco.png')
+    pid_hist = d.factory.hist1d(np.array([]), bins=np.arange(0.5, 160.5, 1))
+    occu = {k : 0 for k in range(1,161)}
+    for f in tqdm.tqdm(files, total=len(files), desc='Creating plot...'):
+        f = up.open(f)
+        event_pids = []
+        vids = f.get('TreeRec').get('Rec').get('hitseries_').get('hitseries_.volume_id_').array()
+        for ev in vids:
+            pids = [vid_hid_map[k] for k in ev if k < 200000000]
+            event_pids.extend(pids)
+            for pid in pids:
+                occu[pid] += 1
+        pid_hist.fill(np.array(event_pids))
 
-cm = matplotlib.colormaps['seismic']
-fig, ax = go.tof.visual.tof_projection_xy(occu, cmap=cm)
-fig.savefig('occu-xy.png')
-fig, ax = go.tof.visual.unroll_cbe_sides(paddle_occupancy=occu, cmap=cm)
-fig.savefig('occu-cbe.png')
-fig, ax = go.tof.visual.unroll_cor(paddle_occupancy=occu, cmap=cm)
-fig.savefig('occu-cor.png')
+    #normalize the occupancy,
+    max_occu = 0
+    for k in occu:
+        if occu[k] > max_occu:
+            max_occu = occu[k]
+    for k in occu:
+        occu[k] = occu[k]/max_occu
+        if occu[k] == 0:
+            occu[k] = np.nan
 
+    #plot paddle occupancy,
+    #fig = plt.figure(figsize=lo.FIGSIZE_A4_LANDSCAPE_HALF_HEIGHT)
+    fig = plt.figure()
+    ax  = fig.gca()
+    pid_hist.line(filled=True, alpha=0.4, color='tab:blue')
+    #cb.visual.adjust_minor_ticks(ax)
+    ax.set_ylim(bottom=0)
+    fig.savefig(f'{args.data_id}_pid_hist_reco.png')
+
+    cm = matplotlib.colormaps['gnuplot2']
+    fig, ax = go.tof.visual.tof_projection_xy(occu, cmap=cm)
+    fig.savefig(f'{args.data_id}_occu-xy.png')
+    fig, ax = go.tof.visual.unroll_cbe_sides(paddle_occupancy=occu, cmap=cm)
+    fig.savefig(f'{args.data_id}_occu-cbe.png')
+    fig, ax = go.tof.visual.unroll_cor(paddle_occupancy=occu, cmap=cm)
+    fig.savefig(f'{args.data_id}_occu-cor.png')
