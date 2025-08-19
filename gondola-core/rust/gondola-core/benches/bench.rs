ab80f3c81515e1ccba0da28e7c603359ec2f5a13
Achim Stoessl <achim.stoessl@gmail.com>
2025-08-18 14:55:49 -1000

    WIP - benchmarking internals, remove outdated, jupyter notebooks
    * jupyter notebooks only changed because they were executed.
      FIXME - set up git hook for something like that

--- gondola-core/rust/gondola-core/benches/bench.rs
+++ gondola-core/rust/gondola-core/benches/bench.rs
@@ -30,6 +30,23 @@ fn bench_roll(c: &mut Criterion) {
 
 //----------------------------------------
 
+fn bench_parse_u64_generic(c: &mut Criterion) {
+  let size : usize = 10000;
+  let data = rand_vec::<u8>(size);
+  use gondola_core::io::parsers::parse_u64_new;
+  c.bench_function("parse_u64_generic", |b|
+                    b.iter(|| parse_u64_new(&data,&mut 5000)));
+}
+
+//----------------------------------------
+
+fn bench_parse_u64(c: &mut Criterion) {
+  let size : usize = 10000;
+  let data = rand_vec::<u8>(size);
+  use gondola_core::io::parsers::parse_u64;
+  c.bench_function("parse_u64", |b|
+                    b.iter(|| parse_u64(&data, &mut 5000)));
+}
 
 //---------------------------------------
 
@@ -40,7 +57,7 @@ fn bench_get_max_value_idx(c: &mut Criterion) {
     use gondola_core::tof::algorithms::get_max_value_idx;
     let size = data.len();
     for _ in 0..10000 {
-      let start_idx : usize = rand::thread_rng().gen_range(0..size); 
+      let start_idx : usize = rand::rng().random_range(0..size); 
       let n_idx = size - start_idx;
       get_max_value_idx(&data, start_idx, n_idx);
     }
@@ -62,6 +79,74 @@ fn bench_clean_spikes(c: &mut Criterion) {
   
 //---------------------------------------
 
+//fn bench_search_for_u16(c: &mut Criterion) {
+//  let size : usize = 10000;
+//  let mut data = rand_vec::<u8>(size);
+//  // make sure there is no 170 in there
+//  for k in 0..data.len() {
+//    if data[k] == 170 { 
+//      data[k] = 0;
+//    }
+//  }
+//  // insert the 170,179 in a defined place
+//  //let idx = rand::rng.random_range(0..data.len() - 2);
+//  let idx = data.len() - 2;
+//  data[idx] = 170;
+//  data[idx + 1] = 170;
+//
+//  use gondola_core::io::serialization::search_for_u16;
+//  c.bench_function("search_for_u16", |b|
+//                    b.iter(|| search_for_u16(0xAA, &data, 0)));
+//}
+  
+//---------------------------------------
+
+fn bench_seek_marker_arr(c: &mut Criterion) {
+  let size : usize = 10000;
+  let mut data = rand_vec::<u8>(size);
+  // make sure there is no 170 in there
+  for k in 0..data.len() {
+    if data[k] == 170 { 
+      data[k] = 0;
+    }
+  }
+  // insert the 170,179 in a defined place
+  //let idx = rand::rng.random_range(0..data.len() - 2);
+  let idx = data.len() - 2;
+  data[idx] = 170;
+  data[idx + 1] = 170;
+
+  use gondola_core::io::serialization::seek_marker;
+  c.bench_function("seek_marker_arr", |b|
+                    b.iter(|| seek_marker(&data.as_slice(), 0xAAAA, 0)));
+}
+  
+//---------------------------------------
+
+fn bench_seek_marker_vec(c: &mut Criterion) {
+  let size : usize = 10000;
+  let mut data = rand_vec::<u8>(size);
+  // make sure there is no 170 in there
+  for k in 0..data.len() {
+    if data[k] == 170 { 
+      data[k] = 0;
+    }
+  }
+  // insert the 170,179 in a defined place
+  //let idx = rand::rng.random_range(0..data.len() - 2);
+  let idx = data.len() - 2;
+  data[idx] = 170;
+  data[idx + 1] = 170;
+
+  use gondola_core::io::serialization::seek_marker;
+  c.bench_function("seek_marker_vec", |b|
+                    b.iter(|| seek_marker(&data, 0xAAAA, 0)));
+}
+  
+//---------------------------------------
+
+
+
 //fn bench_rb_waveform_adc_py(c: &mut Criterion) {
 //  use gondola_core::events::RBWaveform;
 //  use gondola_core::random::FromRandom;
@@ -72,10 +157,22 @@ fn bench_clean_spikes(c: &mut Criterion) {
 
 //---------------------------------------
 
+//criterion_group! {
+//  name = benches;
+//  config = Criterion::default().sample_size(500); // Set default sample size to 500
+//  targets = bench_my_function
+//}
+
+//---------------------------------------
+
 criterion_group!(benches,
                  bench_rand_vec,
                  bench_roll,
                  bench_get_max_value_idx,
-                 bench_clean_spikes);
+                 bench_clean_spikes,
+                 bench_parse_u64,
+                 bench_parse_u64_generic,
+                 bench_seek_marker_arr,
+                 bench_seek_marker_vec);
 criterion_main!(benches);
 
