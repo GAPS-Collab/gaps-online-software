1b7fa1573f34f56ce5cbc032cb715ab0e68065e5
Achim Stoessl <achim.stoessl@gmail.com>
2025-08-26 01:11:59 -1000

    FEATURE - includes legacy headers from SimpleDet for compatibility
    * allows to read/write .root files with CEventRec

---
+++ tof/dataclasses/C++/include/sd_legacy_linkdef.hpp
@@ -0,0 +1,7 @@
+#ifdef __CINT__
+#pragma link C++ class CEventBase+;
+#pragma link C++ class CEventRec+;
+#pragma link C++ class CTrackBase+;
+#pragma link C++ class CTrackRec+;
+#pragma link C++ class GRecoHit+;
+#endif
---
+++ tof/dataclasses/C++/src/sd_legacy.cxx
@@ -0,0 +1,30 @@
+#ifdef BUILD_ROOTCOMPONENTS
+
+#include <iostream>
+
+#include "sd_legacy.hpp"
+#include "TChain.h"
+
+ClassImp(GRecoHit);
+ClassImp(CEventBase);
+ClassImp(CTrackBase);
+ClassImp(CEventRec);
+ClassImp(CTrackRec);
+
+
+void gondola::read_sd_legacy_example() {
+  TChain * input_tree = new TChain("TreeRec");
+  input_tree->Add("/srv/gaps/example-data/Run9125.gse5_241213_142800UTC_rec.root");
+  //input_tree->Add(fName.c_str());
+  auto input_event = new CEventRec();
+  input_tree->SetBranchAddress("Rec", &input_event);
+  u64 max_events = input_tree->GetEntries();
+  for (u64 evid=0; evid<max_events; evid++) {
+    input_tree->GetEntry(evid);
+    std::cout << input_event->runNumber_ << std::endl; 
+    //std::cout << input_event->run_number << std::endl; 
+    std::cout << input_event->eventId_ << std::endl; 
+  }
+}
+
+#endif
