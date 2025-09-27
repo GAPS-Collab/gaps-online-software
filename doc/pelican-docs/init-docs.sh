5fc3e2984e865789754a522e1db03c1d471cb6a8
Achim Stoessl <achim.stoessl@gmail.com>
2025-09-27 11:04:12 -1000

    DOC - landing page for version selection/WIP

--- doc/pelican-docs/init-docs.sh
+++ doc/pelican-docs/init-docs.sh
@@ -2,5 +2,12 @@
 
 rye init 
 rye add pelican markdown
-rye run pelican-themes --symlink ../../resources/extern/blue-penguin-dark 
+rye sync 
+
+mkdir pages/static
+cp ../../resources/assets/lelewaa.png pages/static/ 
+cp ../../resources/assets/pakii.webp pages/static/ 
+cp ../../resources/assets/GAPSLOGO_2023_small.png pages/static/ 
+rye run pelican-themes -v --install ../../resources/extern/blue-penguin-dark 
 rye run pelican pages -s pelicanconf.py -t blue-penguin-dark
+
--- doc/pelican-docs/pelicanconf.py
+++ doc/pelican-docs/pelicanconf.py
@@ -1,9 +1,12 @@
 AUTHOR = 'J.A.Stoessl'
 SITENAME = 'gaps-online-software documentation'
 SITEURL = ""
+SITELOGO = "GAPSLOGO_2023_small.png"
 
-PATH = "content"
 
+
+PATH = "pages"
+STATIC_PATHS = ["static"]
 TIMEZONE = 'UTC'
 
 DEFAULT_LANG = 'en'
@@ -29,7 +32,31 @@ SOCIAL = (
     ("Another social link", "#"),
 )
 
-DEFAULT_PAGINATION = 1
+DEFAULT_PAGINATION = 5
+
+# penguin dark theme 
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+
+
+MENU_INTERNAL_PAGES = (
+    #('Tags', TAGS_URL, TAGS_SAVE_AS),
+    #('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    #('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+    ('GitHub', 'https://github.com/GAPS-Collab/gaps-online-software'),
+    #('Linux Kernel', 'https://www.kernel.org/'),
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
