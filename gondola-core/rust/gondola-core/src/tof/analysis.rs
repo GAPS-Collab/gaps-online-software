a4f6878bfd134d907313c71c766463791ca658c8
Achim Stoessl <achim.stoessl@gmail.com>
2025-12-25 03:03:42 -1000

    FEATURE - gcu variables in tof analysis

--- gondola-core/rust/gondola-core/src/tof/analysis.rs
+++ gondola-core/rust/gondola-core/src/tof/analysis.rs
@@ -46,6 +46,9 @@ pub struct TofAnalysisCache {
   pub edep       : Vec<f32>,
   // energy deposition per panel
   pub edep_panel : HashMap<u8,Vec<f32>>,
+  pub edep_cbe   : Vec<f32>,
+  pub edep_cor   : Vec<f32>,
+  pub edep_umb   : Vec<f32>,
 }
 
 impl TofAnalysisCache {
@@ -73,6 +76,9 @@ impl TofAnalysisCache {
       t_diff     : Vec::<f32>::new(),
       ph_delay   : Vec::<f32>::new(),
       edep       : Vec::<f32>::new(),
+      edep_cbe   : Vec::<f32>::new(),
+      edep_cor   : Vec::<f32>::new(),
+      edep_umb   : Vec::<f32>::new(),
       edep_panel : panel_edeps,
     }
   }
@@ -95,6 +101,9 @@ impl TofAnalysisCache {
     self.t_diff    .clear();
     self.ph_delay  .clear();
     self.edep      .clear();
+    self.edep_cbe  .clear();
+    self.edep_umb  .clear();
+    self.edep_cor  .clear();
     for k in 1..22u8 {
       self.edep_panel.get_mut(&k).unwrap().clear();
     }
@@ -119,7 +128,10 @@ impl TofAnalysisCache {
       "t_diff", 
       "phase_delay",
       "edep",
-      "edep_panel"]
+      "edep_panel",
+      "edep_umb",
+      "edep_cor",
+      "edep_cbe"]
   }
 }
 
@@ -145,6 +157,9 @@ impl TofAnalysisCache {
     self.t_diff    .extend_from_slice(&other.t_diff    ); 
     self.ph_delay  .extend_from_slice(&other.ph_delay  ); 
     self.edep      .extend_from_slice(&other.edep      );
+    self.edep_umb  .extend_from_slice(&other.edep_umb  );
+    self.edep_cor  .extend_from_slice(&other.edep_cbe  );
+    self.edep_cbe  .extend_from_slice(&other.edep_cor  );
     for k in 1..22u8 {
       self.edep_panel.get_mut(&k).unwrap().extend_from_slice(&other.edep_panel.get(&k).unwrap());
     }
@@ -289,6 +304,27 @@ impl TofAnalysisCache {
       }
       "edep" => {
         let slice = &self.edep[..];
+        // this is supposed to be readonly
+        // FIXME - check this!
+        let py_array = PyArray1::from_slice(py, slice);
+        return Some(py_array);
+      }
+      "edep_umb" => {
+        let slice = &self.edep_umb[..];
+        // this is supposed to be readonly
+        // FIXME - check this!
+        let py_array = PyArray1::from_slice(py, slice);
+        return Some(py_array);
+      }
+      "edep_cor" => {
+        let slice = &self.edep_cor[..];
+        // this is supposed to be readonly
+        // FIXME - check this!
+        let py_array = PyArray1::from_slice(py, slice);
+        return Some(py_array);
+      }
+      "edep_cbe" => {
+        let slice = &self.edep_cbe[..];
         // this is supposed to be readonly
         // FIXME - check this!
         let py_array = PyArray1::from_slice(py, slice);
@@ -744,6 +780,9 @@ pub struct TofAnalysis {
   pub hg_mapping    : DsiJChPidMapping,
   // caches 
   pub c_hit         : Vec<u8>,
+  pub c_hit_umb     : Vec<u8>,
+  pub c_hit_cor     : Vec<u8>,
+  pub c_hit_cbe     : Vec<u8>,
   pub c_thit        : Vec<u8>,
   pub c_rblink      : Vec<u8>,
   pub c_miss_hit    : Vec<u8>,   
@@ -760,6 +799,9 @@ impl TofAnalysis {
 
   fn clear_hit_stats(&mut self) {
     self.c_hit      .clear();
+    self.c_hit_cbe  .clear();
+    self.c_hit_umb  .clear();
+    self.c_hit_cor  .clear();
     self.c_thit     .clear();
     self.c_rblink   .clear();
     self.c_miss_hit .clear();   
@@ -809,6 +851,9 @@ impl TofAnalysis {
       c_miss_hit    : Vec::<u8>::new(),
       c_nc_pid      : Vec::<u8>::new(),
       c_hit         : Vec::<u8>::new(),
+      c_hit_umb     : Vec::<u8>::new(),
+      c_hit_cor     : Vec::<u8>::new(),
+      c_hit_cbe     : Vec::<u8>::new(),
       c_thit        : Vec::<u8>::new(),
       c_rblink      : Vec::<u8>::new(),
       cache         : TofAnalysisCache::new(),
@@ -864,6 +909,7 @@ impl TofAnalysis {
       //print (ev)
     }
     ev.normalize_hit_times();
+    ev.calc_gcu_variables();
 
     //# before cutting, calculate missing hits
     //# the problem for removing hits right now
@@ -978,10 +1024,15 @@ impl TofAnalysis {
     } else if nhit_ev > nhit_t_ev {
       self.extra_hits += 1;
     }
+    self.c_hit_umb.push(ev.n_hits_umb);
+    self.c_hit_cor.push(ev.n_hits_cor);
+    self.c_hit_cbe.push(ev.n_hits_cbe);
     self.c_hit.push(nhit_ev);
     self.c_thit.push(nhit_t_ev);
     self.c_rblink.push(n_rblink_ev);
-
+    self.cache.edep_umb.push(ev.tot_edep_umb);
+    self.cache.edep_cor.push(ev.tot_edep_cor);
+    self.cache.edep_cbe.push(ev.tot_edep_cbe);
     if !self.beta_analysis {
       return
     }
@@ -1067,6 +1118,9 @@ impl TofAnalysis {
 
   fn add_other_hit_cache(&mut self, other :  &TofAnalysis) {
     self.c_hit     .extend_from_slice(&other.c_hit);
+    self.c_hit_umb .extend_from_slice(&other.c_hit_umb);
+    self.c_hit_cor .extend_from_slice(&other.c_hit_cor);
+    self.c_hit_cbe .extend_from_slice(&other.c_hit_cbe);
     self.c_thit    .extend_from_slice(&other.c_thit);
     self.c_rblink  .extend_from_slice(&other.c_rblink);
     self.c_miss_hit.extend_from_slice(&other.c_miss_hit);   
@@ -1100,6 +1154,39 @@ impl TofAnalysis {
   #[getter]
   fn c_hit_py<'py>(&'py self, py: Python<'py>) -> PyResult<Bound<'py, PyArray1<u8>>> {
     let slice = &self.c_hit[..];
+    // this is supposed to be readonly
+    // FIXME - check this!
+    let py_array = PyArray1::from_slice(py, slice);
+    return Ok(py_array);
+  }
+  
+  /// This is the number of hits/event for each seen event
+  #[pyo3(name="c_hit_cbe")]
+  #[getter]
+  fn c_hit_cbe_py<'py>(&'py self, py: Python<'py>) -> PyResult<Bound<'py, PyArray1<u8>>> {
+    let slice = &self.c_hit_cbe[..];
+    // this is supposed to be readonly
+    // FIXME - check this!
+    let py_array = PyArray1::from_slice(py, slice);
+    return Ok(py_array);
+  }
+  
+  /// This is the number of hits/event for each seen event
+  #[pyo3(name="c_hit_umb")]
+  #[getter]
+  fn c_hit_umb_py<'py>(&'py self, py: Python<'py>) -> PyResult<Bound<'py, PyArray1<u8>>> {
+    let slice = &self.c_hit_umb[..];
+    // this is supposed to be readonly
+    // FIXME - check this!
+    let py_array = PyArray1::from_slice(py, slice);
+    return Ok(py_array);
+  }
+  
+  /// This is the number of hits/event for each seen event
+  #[pyo3(name="c_hit_cor")]
+  #[getter]
+  fn c_hit_cor_py<'py>(&'py self, py: Python<'py>) -> PyResult<Bound<'py, PyArray1<u8>>> {
+    let slice = &self.c_hit_cor[..];
     // this is supposed to be readonly
     // FIXME - check this!
     let py_array = PyArray1::from_slice(py, slice);
