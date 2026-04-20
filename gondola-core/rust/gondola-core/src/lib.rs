5d0cca7c0ed70f37d2f2445af36e91bce9c894c7
Achim Stoessl <achim.stoessl@gmail.com>
2026-04-19 16:04:50 -1000

    REFACTOR - move transfer fn to its own module
    * allow to read transfer functions from file and
      also to save them to the db

--- gondola-core/rust/gondola-core/src/lib.rs
+++ gondola-core/rust/gondola-core/src/lib.rs
@@ -434,6 +434,7 @@ fn db_py<'_py>(m: &Bound<'_py, PyModule>) -> PyResult<()> {
   m.add_function(wrap_pyfunction!(load_calibration_db_elena, m)?)?;
   m.add_function(wrap_pyfunction!(create_trk_mask_table, m)?)?;
   m.add_function(wrap_pyfunction!(create_trk_pedestal_table, m)?)?;
+  m.add_function(wrap_pyfunction!(create_trk_transfer_fn_table, m)?)?;
   Ok(())
 }
 
