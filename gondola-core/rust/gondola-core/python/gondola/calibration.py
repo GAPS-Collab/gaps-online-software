625b19c19790bdb30872b64cb579f608fa519732
Achim Stoessl <achim.stoessl@gmail.com>
2026-04-21 11:46:46 -1000

    FEATURE - one-shot-plot transfer functions

--- gondola-core/rust/gondola-core/python/gondola/calibration.py
+++ gondola-core/rust/gondola-core/python/gondola/calibration.py
@@ -5,8 +5,10 @@ Calibration related methods
 from pathlib import Path
 import re
 import tqdm
+import numpy as np
 
 from . import _gondola_core 
+from .visual.style import gander_line_plot 
 
 RBCalibrations = _gondola_core.calibration.RBCalibrations
 RBCalibrations.__module__ = __name__ 
@@ -49,3 +51,17 @@ def load_rb_calibrations(cali_dir : Path, load_event_data = False):
         calibs[rb_id] = cali 
     return calibs
 
+def _plot_transfer_fn(self,strip_id):
+    """
+    Plot the transfer function for the respective strip over a 
+    reasonable ADC range (0-1600) 
+    """
+    if strip_id in self.tf_map.keys():
+        transfer_fn = self.tf_map[strip_id].transfer_fn 
+        xs = np.arange(0,1600,1)
+        ys = np.array([transfer_fn(x) for x in xs])
+        fig = gander_line_plot(xs,ys, title=f'Transfer Fn strip {strip_id}', xlabel='ADC', ylabel='mV')
+        return fig
+    print(f"Unable to plot transfer fn for {strip_id}. Are the transfer functions loaded/set?")
+
+TrackerOfflineCalibration.plot_transfer_fn = _plot_transfer_fn
